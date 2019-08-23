faf180235ff74bb69fc8b79adf81308e55a4ab79
MICHAEL WILSON <wilsonmi@otc.edu>
2019-08-23 15:55:23 -0500

    Start working on Weather API

---
+++ Entertianment Website - Javascript/weatherScript.js
@@ -0,0 +1,11 @@
+ //Script for Weather API -------/////////////////////////////////////////////////
+    
+ //var httpRequest = new XMLHttpRequest(); //built our httpRequest object
+
+
+function writeHello()
+{
+ alert("Page has loaded");
+}
+
+window.onload = writeHello
