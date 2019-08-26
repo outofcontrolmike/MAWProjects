2953b1c18357d42b17a212c9a284548728758b50
Michael Wilson <outofcontrolmike@gmail.com>
2019-08-26 15:07:30 -0500

    Made an API Call, Now I need to display it to web

--- Entertianment Website - Javascript/weatherScript.js
+++ Entertianment Website - Javascript/weatherScript.js
@@ -1,11 +1,30 @@
- //Script for Weather API -------/////////////////////////////////////////////////
-    
- //var httpRequest = new XMLHttpRequest(); //built our httpRequest object
+// Connect to an API
+
+// create xmlHttp object
+    //get URL
+        //Create a function for JSON
+            // Send the request
 
 
-function writeHello()
-{
- alert("Page has loaded");
+
+
+var weather = document.createElement('p');
+weather.setAttribute("id", "Div1");
+
+var weatherInfo = document.getElementById('Div1');
+
+
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://api.darksky.net/forecast/b07431fb31f0d5dce323ac70c78103d1/37.1792956,-93.2923', true);
+
+request.onload = function () {
+    //Access JSON
+    var data = JSON.parse(this.response)
+   console.log(data);
+
+   weatherInfo.innerHTML = data;
+   
+
 }
-
-window.onload = writeHello
+request.send();
