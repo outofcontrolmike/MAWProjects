703db6cfde041d14c3a5c8d8cb54a9c86cfdbea9
MAW_CIS_Student <mw0415436@otc.edu>
2020-07-10 22:25:58 -0500

    Made functional links and some additional styling

--- WilsonBlog/js/script.js
+++ WilsonBlog/js/script.js
@@ -1,1 +1,18 @@
 //functions for my blog
+
+//Alerts
+onload(alert("Testing 123"));
+
+//changing nav bar
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function setRandomColor() {
+    $("#colorpad").css("background-color", getRandomColor());
+  }
