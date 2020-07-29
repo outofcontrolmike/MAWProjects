7f830decfa8c3813b6ed0c3882199a650119aa49
MAW_CIS_Student <mw0415436@otc.edu>
2020-07-28 21:05:37 -0500

    wrote most logic out, stuck on player choice infinte loop

--- Rock Paper Scissors/Rock Paper Scissors/Program.cs
+++ Rock Paper Scissors/Rock Paper Scissors/Program.cs
@@ -4,8 +4,8 @@ namespace Rock_Paper_Scissors
 {
     class Program
     {
+        static string pName = "Hank Hill";
         static string name = "Rob";
-        static bool one_two = true;
         static string choice = "0";
         static int pWin = 0;
         static int cWin = 0;
@@ -14,8 +14,7 @@ namespace Rock_Paper_Scissors
         static void Main(string[] args)
         {
             getChoice();
-            Console.WriteLine("Testing");
-
+            game();
         }
 
         /// <summary>
@@ -23,80 +22,206 @@ namespace Rock_Paper_Scissors
         /// </summary>
         public static void getChoice()
         {
-            firstQuestion();
-            askName();
+            FirstQuestion();
+            AskName();
         }
 
         public static void game()
         {
+            while(pWin < 3 || cWin < 3)
+            {
+                Console.WriteLine("The match begins!");
+                Console.ReadKey();
+                Console.Clear();
+                PMove();
+                CMove();
+                GameChecker();
+                Console.ReadKey();
+            }
+            Console.WriteLine("Game is over!");
+            int pDifference = pWin - cWin;
+            int cDifference = cWin - pWin;
 
+            if(pWin > cWin)
+            {
+                Console.WriteLine("Congratulations, you won by " + pDifference + "!");
+                Console.WriteLine("Final score:" +
+                    pName + ": " + pWin + "\n" +
+                   name + ": " + cWin);
+                Console.WriteLine("\nEnd of game");
+                Console.WriteLine("\n****************************************************");
+                
+               
+            }
+            if (cWin > pWin)
+            {
+                Console.WriteLine("Too Bad!!! the computer won by " + cDifference + "!");
+                Console.WriteLine("Final score:" +
+                    name + ": " + cWin + "\n" +
+                   pName + ": " + pWin);
+                Console.WriteLine("\nEnd of game");
+                Console.WriteLine("\n****************************************************");
+
+            }
         }
         /// <summary>
         ///Decides Player's move
         /// </summary>
-        public static void pMove()
+        public static void CMove()
         {
-            string num = "0";
-            while (num!= "1" || num != "2" || num != "3")
+            int num = 0;
+            while (num!= 1 || num != 2 || num != 3)
             {
-                Console.WriteLine("Press 1 for Rock" +
-             "Press 2 for Paper" +
-             "Press 3 for Scissors");
-                num = Console.ReadLine();
-                if (num == "1")
+                System.Random random = new System.Random();
+
+                num = random.Next(1, 4);
+                if (num == 1)
                 {
-                    pChoice = "Rock";
+                    cChoice = "Rock";
                     
                 }
-                if(num == "2")
+                if(num == 2)
                 {
-                    pChoice = "Paper";
-                    pChoice = "Paper";
+                    cChoice = "Paper";
                 }
-                if(num == "3")
+                if(num == 3)
                 {
-                    pChoice = "Scissors";
+                    cChoice = "Scissors";
                 }
                 else
                 {
                     Console.WriteLine("Please enter 1, 2 or 3 as an option");
                 }
             }
-            Console.WriteLine("You chose: " + pChoice);
-           
+            Console.WriteLine("Computer Chose: " + cChoice);
+            Console.ReadKey();
         }
 
         /// <summary>
         /// Will auto generate the computer's decision
         /// </summary>
-        public static void cMove()
+        public static void PMove()
         {
+            string num = "0";
+            while (num != "1" || num != "2" || num != "3")
+            {
+                Console.WriteLine("Press 1 for Rock\n" +
+             "Press 2 for Paper\n" +
+             "Press 3 for Scissors");
+                num = Console.ReadLine();
+                if (num == "1")
+                {
+                    pChoice = "Rock";
+                    break;
+
+                }
+                if (num == "2")
+                {
+                    pChoice = "Paper";
+                    break;
+                }
+                if (num == "3")
+                {
+                    pChoice = "Scissors";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 1, 2 or 3 as an option");
+                }
+            }
+            Console.WriteLine("\nyou chose: " + pChoice);
+        }
+
+       public static void Draw()
+        {
+            //rock rock  = draw
+            //pap  pap   = draw
+            //scis scis  = draw
+            if (cChoice == "Rock" && pChoice == "Rock" || cChoice == "Paper" && pChoice == "Paper" || cChoice == "Scissors" && pChoice == "")
+            {
+                Console.WriteLine("Draw between you and the oponnet\n");
+                Console.ReadKey();
+            }// end draw
+          
+        }
+
+        /// <summary>
+        /// Players loss scenarios
+        /// </summary>
+        public static void LoseMatch()
+        {
+            if(pChoice == "Rock" && cChoice == "Paper")
+            {
+                Console.WriteLine("Paper covers rock\n" +
+                    "You lose this match");
+                cWin++;
+                Console.ReadKey();
+            }
+            if (pChoice == "Paper" && cChoice == "Scissors")
+            {
+                Console.WriteLine("Sissors cuts paper\n" +
+                    "You lose this match");
+                cWin++;
+                Console.ReadKey();
+            }
+            if (pChoice == "Scissors" && cChoice == "Rock")
+            {
+                Console.WriteLine("Rock beats paper down\n" +
+                    "You lose this match");
+                cWin++;
+                Console.ReadKey();
+
+            }
+            Console.Clear();
 
         }
 
-       
-        public static void gameChecker()
+        /// <summary>
+        /// Players Win scenarios
+        /// </summary>
+        public static void WinMatch()
         {
-            //check all scenarios of rock paper scissors
-            //rock rock  = draw
-            //scis scis  = draw
-            //pap  pap   = draw
-            //rock paper = win
-            //paper rock = win
-            //scis paper = win
-            //rock paper = lose
-            //scis rock  = lose
-            //paper scis = lose
+            if (cChoice == "Rock" && pChoice == "Paper")
+            {
+                Console.WriteLine("Paper covers rock\n" +
+                    "You win the match!");
+                Console.ReadKey();
+                pWin++;
+            }
+            if (cChoice == "Paper" && pChoice == "Scissors")
+            {
+                Console.WriteLine("Sissors cuts paper\n" +
+                    "You win the match!");
+                Console.ReadKey();
+                pWin++;
+            }
+            if (cChoice == "Scissors" && pChoice == "Rock")
+            {
+                Console.WriteLine("Rock beats paper down\n" +
+                    "You win the match!");
+                Console.ReadKey();
+                pWin++;
+            }
+            Console.Clear();
 
-            // if Choice
 
+        }
+        /// <summary>
+        /// Checks to see who won the match
+        /// </summary>
+        public static void GameChecker()
+        {
+            Draw();
+            LoseMatch();
+            WinMatch();
         }
 
       
         /// <summary>
         /// Goes through initial menu
         /// </summary>
-        public static void firstQuestion()
+        public static void FirstQuestion()
         {
             while (choice != "1" || choice != "2")
             {
@@ -131,10 +256,10 @@ namespace Rock_Paper_Scissors
         /// <summary>
         /// Asks's user and computer name
         /// </summary>
-        public static void askName()
+        public static void AskName()
         {
             Console.WriteLine("Please enter your name!");
-            string pName = Console.ReadLine();
+            pName = Console.ReadLine();
             Console.WriteLine("Okay " + pName + "!  Do you want to name your opponent?\n" +
                 "type yes or no");
             string choice = Console.ReadLine();
@@ -159,7 +284,7 @@ namespace Rock_Paper_Scissors
         /// <summary>
         /// Prints rules
         /// </summary>
-        public static void printRules()
+        public static void PrintRules()
         {
             Console.WriteLine("No rules yet");
             Console.ReadKey();
