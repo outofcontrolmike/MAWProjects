3fbe0dc4011b3ee13f00f1f78b223e7af0cd4a42
MAW_CIS_Student <mw0415436@otc.edu>
2020-08-17 19:24:03 -0500

    testing

---
+++ BrewGlobal/BrewGlobal/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BrewGlobal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thank you for chosing Brew Global!");
+        }
+    }
+}
