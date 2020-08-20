7d49ee3a20d0ee4b100fe6bbfa92ee66fc86f960
MAW_CIS_Student <mw0415436@otc.edu>
2020-08-19 19:49:52 -0500

    Created functions class

---
+++ BrewGlobal/BrewGlobal/functions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrewGlobal
+{
+    /// <summary>
+    //Useful functions for when things get complex
+    /// </summary>
+    class functions
+    {
+    }
+}
