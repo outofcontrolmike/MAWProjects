4f7a190727ed289b7216d86fc43713368b3f308e
MAW_CIS_Student <mw0415436@otc.edu>
2020-10-26 22:09:59 -0500

    Great race is finished

--- Great Race - JavaScript/race.js
+++ Great Race - JavaScript/race.js
@@ -3,6 +3,13 @@ var marioRun = document.getElementById("mario");
 var yoshiRun = document.getElementById("yoshi");
 var yoshiWin = document.getElementById("yoshiWin");
 var marioWin = document.getElementById("winMario");
+var star = document.getElementById('star');
+var header = document.getElementById('header');
+
+var marioTotal = document.getElementById('marioScore');
+var yoshiTotal = document.getElementById('yoshiScore');
+var marioScore = 0;
+var yoshiScore = 0;
 
 // onlick event for starting race by clicking stopLight variable
 document.getElementById("star").addEventListener("click", startRace);
@@ -18,13 +25,14 @@ var myTime;
 
 // handle starting the race
 function startRace() {
-  document.getElementById("star").src = "on.png";
+  star.src = "on.png";
+  star.removeEventListener("click",startRace);
   marioRun = document.getElementById("mario").src = "mariorun.png";
   yoshiRun = document.getElementById("yoshi").src = "yoshirun.png";
   document.getElementById("mario").style.left = "0px";
   document.getElementById("yoshi").style.left = "0px";
   console.log("testing");
-  myTime = setInterval(takeOff, 10);
+  myTime = setInterval(takeOff, 6);
   takeOff();
 
 }
@@ -45,16 +53,20 @@ function startRace() {
       document.getElementById("yoshi").style.visibility = "hidden";
       document.getElementById("mario").style.visibility = "hidden";
       document.getElementById("winMario").style.visibility = "visible";
-      return;
+      marioScore++;
+      marioTotal.innerHTML = marioScore;
     } else if (parseInt(yoshiRacer) >= 1600) {
       myStop();
       document.getElementById("yoshi").style.visibility = "hidden";
       document.getElementById("mario").style.visibility = "hidden";
       document.getElementById("yoshiWin").style.visibility = "visible";
-
+      yoshiScore++;
+      yoshiTotal.innerHTML = yoshiScore;
     }
+
     yoshiWin.addEventListener("click", revealImages);
     marioWin.addEventListener("click", revealImages);
+    header.innerHTML = "Click the winner to restart the race";
   }
 
 
@@ -65,11 +77,14 @@ function startRace() {
     document.getElementById("winMario").style.visibility = "hidden";
     document.getElementById("yoshiWin").style.visibility = "hidden";
     document.getElementById("star").src = "off.png";
+    header.innerHTML = 'ready for another round?';
+
   }
 
   // This stops the interval
   function myStop() {
     clearInterval(myTime);
+
   }
 
   // this function will style the oriiginal back to the left and insert the running images.
