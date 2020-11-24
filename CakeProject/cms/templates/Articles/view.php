00a1363d92c605ba82e3b871b5bd2063fed9cf44
MAW_CIS_Student <mw0415436@otc.edu>
2020-11-23 20:09:55 -0600

    had to start over

--- CakeProject/cms/templates/Articles/view.php
+++
@@ -1,6 +0,0 @@
-<!-- File: templates/Articles/view.php -->
-
-<h1><?= h($article->title) ?></h1>
-<p><?= h($article->body) ?></p>
-<p><small>Created: <?= $article->created->format(DATE_RFC850) ?></small></p>
-<p><?= $this->Html->link('Edit', ['action' => 'edit', $article->slug]) ?></p>
