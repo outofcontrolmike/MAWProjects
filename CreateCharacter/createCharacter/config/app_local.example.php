d79e064c9304554c6fa6acdacfb1930b1d8c3758
Michael Wilson <outofcontrolmike@gmail.com>
2021-02-16 20:28:21 -0600

    Connected database

--- CreateCharacter/createCharacter/config/app_local.example.php
+++ CreateCharacter/createCharacter/config/app_local.example.php
@@ -44,10 +44,10 @@ return [
              */
             //'port' => 'non_standard_port_number',
 
-            'username' => 'my_app',
-            'password' => 'secret',
+            'username' => 'root',
+            'password' => '',
 
-            'database' => 'my_app',
+            'database' => 'test',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
@@ -66,9 +66,9 @@ return [
         'test' => [
             'host' => 'localhost',
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'test_myapp',
+            'username' => 'root',
+            'password' => '',
+            'database' => '',
             //'schema' => 'myapp',
             'url' => env('DATABASE_TEST_URL', null),
         ],
