28e169cb7f725194ec2714043915b20fdd3f8beb
Michael Wilson <outofcontrolmike@gmail.com>
2021-06-16 20:40:15 -0500

    Adding brewery project

---
+++ brewery_Fetch/script.js
@@ -0,0 +1,176 @@
+//count variable
+let brewCount = 0;
+
+//Fetch breweries
+document.getElementById('submitFetch').addEventListener('click', grabFilters);
+
+//Get breweries
+function grabFilters() {
+let input = document.getElementById('inputValue').value;
+
+let filter = document.getElementById('filter');
+let filterValue = filter.options[filter.selectedIndex].value;
+
+let stateFilter = document.getElementById('stateFilter');
+let stateFilterValue = stateFilter.options[stateFilter.selectedIndex].innerHTML
+
+let brewType = document.getElementById('brewType')
+let brewValue = brewType.options[brewType.selectedIndex].value;
+
+getBreweries(input,filterValue,stateFilterValue,brewValue);
+}
+
+//get breweries
+function getBreweries(input,optionValue, stateValue, brewValue) {
+let filter = optionValue.toLowerCase();
+let stateFilter = "";
+let brewFilter = "";
+
+if(brewValue != "Brewery Type")
+{
+    brewFilter ='&by_type='+brewValue;
+}
+else {
+    brewFilter ="";
+}
+
+//
+if(stateValue != "Select a State" && stateValue != "All")
+{
+    stateFilter = '&by_state='+stateValue;
+}
+if(input != parseInt(input)) {
+
+fetch('https://api.openbrewerydb.org/breweries?by_'+filter+"="+input+brewFilter+stateFilter+'&per_page=50')
+.then(response => response.json())
+.then((brew) => {
+    createBrew(brew);
+}
+)
+.catch((error) => {
+    console.error('Error:', error);
+    console.log("error");
+});
+;
+}
+else {
+alert('Please enter a value containing letters.')
+}
+}
+
+function createBrew(brew) {
+document.getElementById('breweries').innerHTML = "";
+brewCount = 0;
+brew.forEach(createCard)
+document.getElementById('count').innerHTML = brewCount;
+
+}
+
+function createCard(item)
+{
+
+brewCount++;
+
+// Create a bunch of elements for each card
+let container = document.createElement('div');
+let column1 = document.createElement('div');
+let column2 = document.createElement('div');
+let divider = document.createElement('hr');
+let breweryName = document.createElement('p');
+let breweryStreet = document.createElement('p');
+let breweryCity = document.createElement('p');
+let breweryState = document.createElement('p');
+let breweryPostal = document.createElement('p');
+let breweryCountry = document.createElement('p');
+let breweryType = document.createElement('span');
+let breweryPhone = document.createElement('p');
+let breweryURL = document.createElement('a');
+
+//icon elements
+let street,city,state,postal,country,phone,url;
+
+street = document.createElement('i');
+city = document.createElement('i');
+state = document.createElement('i');
+postal = document.createElement('i');
+country = document.createElement('i');
+phone = document.createElement('i');
+url = document.createElement('i');
+
+//find a way to pass all these in to a function that checks if empty
+breweryURL.href = item.website_url;
+
+if(breweryURL != "http://localhost:3000/null")
+{
+breweryURL.innerHTML = breweryURL;
+}
+else {
+    breweryURL.innerHTML = "N/A";
+    breweryURL.id = "disableLink";
+}
+
+breweryStreet.innerHTML = item.street;
+breweryName.innerHTML = item.name;
+breweryCity.innerHTML = item.city;
+breweryState.innerHTML = item.state;
+breweryPostal.innerHTML = item.postal_code;
+breweryCountry.innerHTML = item.country;
+breweryType.innerHTML = item.brewery_type;
+breweryPhone.innerHTML = item.phone;
+
+
+//Additional styling
+
+container.className = "ui segment two column grid container stackable";
+container.id = "brewCard";
+column1.className = "column";
+column2.className = "column";
+breweryName.className = "ui header text massive container";
+breweryName.id ="header";
+breweryType.className = "floatMe";
+breweryType.style.color = "red";
+
+//add icons
+city.className = "city icon";
+street.className = "road icon";
+state.className = "gopuram icon";
+postal.className = "mail bulk icon";
+country.className = "globe icon";
+phone.className = "phone alternate icon";
+url.className = "linkify icon";
+
+breweryName.append(breweryType,divider)
+
+container.append(breweryName,column1,column2);
+
+//prepend
+breweryStreet.prepend(street);
+breweryCity.prepend(city);
+breweryState.prepend(state);
+breweryPostal.prepend(postal);
+breweryCountry.prepend(country);
+breweryPhone.prepend(phone);
+breweryURL.prepend(url);
+
+column1.append(breweryStreet,breweryCity,breweryState,breweryPostal);
+column2.append(breweryCountry,breweryPhone,breweryURL)
+document.getElementById('breweries').append(container);
+}
+
+//Set color with color picker
+let chosenColor = document.getElementById('colorpicker');
+
+function chooseColor() {
+let grabIcons = document.getElementsByTagName('i');
+
+
+for(let  i = 0; i <= grabIcons.length; i++)
+{
+grabIcons[i].style.color = chosenColor.value;
+
+}
+
+}
+chosenColor.addEventListener("input", chooseColor);
+let countColor = document.getElementById('count');
+countColor.style.color = chosenColor.value;
