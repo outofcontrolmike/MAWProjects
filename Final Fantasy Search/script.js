2402681e385571522ffdeddbd12f076700468500
Michael Wilson <outofcontrolmike@gmail.com>
2021-07-04 11:07:29 -0500

    got the name appending to list

--- Final Fantasy Search/script.js
+++ Final Fantasy Search/script.js
@@ -6,7 +6,12 @@ let searchValue = document.getElementById('searchInput').value;
 
 fetch('https://www.moogleapi.com/api/v1/characters/search?name=' + searchValue + '')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => 
+  createCard(data));
   alert('You are looking for : ' + searchValue);
+}
 
+function createCard(data) {
+console.log("CreateCard: " + data[0].name);
+document.getElementById('list').innerHTML = data[0].name
 }
