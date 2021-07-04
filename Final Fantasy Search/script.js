1e32964dc53035e3f4661318f6ba23175a89a5f5
Michael Wilson <outofcontrolmike@gmail.com>
2021-07-04 13:52:36 -0500

    Able to display multiple characters now

--- Final Fantasy Search/script.js
+++ Final Fantasy Search/script.js
@@ -7,15 +7,21 @@ let searchValue = document.getElementById('searchInput').value;
 fetch('https://www.moogleapi.com/api/v1/characters/search?name=' + searchValue + '')
   .then(response => response.json())
   .then(data => 
-  createCard(data[0]));
+  createCard(data));
     alert('You are looking for : ' + searchValue);
 }
 
-function createCard(data) {
-  data.map(getFFData(data));  
+function createCard(data) 
+{
+  data.map(getFFData);  
 }
 
 function getFFData(character) {
   console.log(character.name + character.race);
-  
+  let card = document.createElement('div');
+  card.className = "ui segment";
+  let name = character.name;
+  let race = character.race;
+  card.append(name,race);
+  document.getElementById('list').append(card);
 }
