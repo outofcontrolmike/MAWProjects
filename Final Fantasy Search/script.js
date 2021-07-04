37322bc17d930949b20c889b5d855934aa5fbc30
Michael Wilson <outofcontrolmike@gmail.com>
2021-07-04 13:39:23 -0500

    Trying to get mapping through objects to work

--- Final Fantasy Search/script.js
+++ Final Fantasy Search/script.js
@@ -7,11 +7,15 @@ let searchValue = document.getElementById('searchInput').value;
 fetch('https://www.moogleapi.com/api/v1/characters/search?name=' + searchValue + '')
   .then(response => response.json())
   .then(data => 
-  createCard(data));
-  alert('You are looking for : ' + searchValue);
+  createCard(data[0]));
+    alert('You are looking for : ' + searchValue);
 }
 
 function createCard(data) {
-console.log("CreateCard: " + data[0].name);
-document.getElementById('list').innerHTML = data[0].name
+  data.map(getFFData(data));  
+}
+
+function getFFData(character) {
+  console.log(character.name + character.race);
+  
 }
