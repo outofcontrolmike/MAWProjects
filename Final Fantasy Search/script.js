f414e7f9ef10d9c81932de74bf91103ea79aedfa
Michael Wilson <outofcontrolmike@gmail.com>
2021-07-04 09:46:39 -0500

    Instantiated files and wrote html basic elements

--- Final Fantasy Search/script.js
+++ Final Fantasy Search/script.js
@@ -1,15 +1,2 @@
 
 
-var fetcher = fetch('https://www.moogleapi.com/api/v1/characters')
-.then(response => {
-  if (!response.ok) {
-    throw new Error("Network response was not okay");
-  }
-  return response.json();
-})
-
-console.table(fetcher);
-
-function createCharacters() {
-    let character = "";
-}
