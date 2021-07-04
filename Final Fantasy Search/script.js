27e30a89ca77c32ea63fec236eb6276bae9f66e6
Michael Wilson <outofcontrolmike@gmail.com>
2021-07-04 10:36:39 -0500

    got the basic search working

--- Final Fantasy Search/script.js
+++ Final Fantasy Search/script.js
@@ -1,2 +1,12 @@
+document.getElementById('onSubmit').addEventListener('click', submitRequest);
+
+function submitRequest() {
+let searchValue = document.getElementById('searchInput').value;
 
 
+fetch('https://www.moogleapi.com/api/v1/characters/search?name=' + searchValue + '')
+  .then(response => response.json())
+  .then(data => console.log(data));
+  alert('You are looking for : ' + searchValue);
+
+}
