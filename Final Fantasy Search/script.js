2ee7aac885352b76081fbc6d1f5247eccbd3f3c5
Michael Wilson <outofcontrolmike@gmail.com>
2021-07-05 17:04:23 -0500

    got npm

--- Final Fantasy Search/script.js
+++ Final Fantasy Search/script.js
@@ -7,8 +7,7 @@ let searchValue = document.getElementById('searchInput').value;
 fetch('https://www.moogleapi.com/api/v1/characters/search?name=' + searchValue + '')
   .then(response => response.json())
   .then(data => 
-  createCard(data));
-    alert('You are looking for : ' + searchValue);
+  createCard(data) + console.log(data));
 }
 
 function createCard(data) 
@@ -16,12 +15,30 @@ function createCard(data)
   data.map(getFFData);  
 }
 
+/*Character Props
+Name
+Japanese Name
+Origin (The game they originated in)
+Race
+Gender
+Age
+Job
+Height
+Weight
+Pictures (an array)
+Stats (an array)
+Description
+*/
 function getFFData(character) {
   console.log(character.name + character.race);
   let card = document.createElement('div');
   card.className = "ui segment";
+
+  
   let name = character.name;
+  let jName = character.japaneseName;
   let race = character.race;
+  
   card.append(name,race);
   document.getElementById('list').append(card);
 }
