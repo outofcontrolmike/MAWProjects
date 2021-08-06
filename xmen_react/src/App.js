b8c326deb0de85339158770992d8224fb04119ed
Michael Wilson <mike@beardeddevops.com>
2021-08-06 15:46:47 -0500

    Was able to map over results for xmen characters succesffuly

--- xmen_react/src/App.js
+++ xmen_react/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Characters from './components/characters'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    //stores output from api
+    characters: []
+  }
+
+
+  componentDidMount() {
+    let url = "https://xmenapiheroku.herokuapp.com/api/characters";
+    fetch(url)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ characters: data.results })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    return (
+      <Characters characters={this.state.characters} />
+    )
+  }
 }
 
 export default App;
