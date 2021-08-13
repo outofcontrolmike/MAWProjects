58390d8d65231c06198e01d2ae53749124bb8d51
Michael Wilson <mike@beardeddevops.com>
2021-08-13 14:35:58 -0500

    fixed media screen for phone

--- WilsonBlog/html/form.php
+++ WilsonBlog/html/form.php
@@ -1,9 +1,8 @@
 
 <head>
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Contact</title>
-
-
   <!-- You MUST include jQuery before Fomantic -->
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
   <link
