bbe65638a87e8b2523111be6b0f527c821da7402
Michael Wilson <outofcontrolmike@gmail.com>
2021-08-22 15:32:40 -0500

    ColorSetting works for each pokemon

--- PokemonAPI/pokeonapi2.js
+++ PokemonAPI/pokeonapi2.js
@@ -12,33 +12,33 @@ var prevUrl = "";
 
 
 //SUPER IMPORTANT
-document.getElementById('searchValue').addEventListener('change',fetchStatus);
+document.getElementById('searchValue').addEventListener('change', fetchStatus);
 
 
 let search = document.getElementById('search');
-search.addEventListener('click',catchSingle)
+search.addEventListener('click', catchSingle)
 
 let collection = document.getElementById('collection');
-collection.addEventListener('change',fetchStatus);
+collection.addEventListener('change', fetchStatus);
 
 let single = document.getElementById('name');
-single.addEventListener('change',fetchStatus);
+single.addEventListener('change', fetchStatus);
 
 
 
 //event listeners
 var searchInput = document.getElementById('searchValue');
 
- var dayInvert= document
+var dayInvert = document
   .getElementById("day")
-  .addEventListener("click",invertColors);
+  .addEventListener("click", invertColors);
 
 
 document.getElementById("clear").addEventListener("click", clearList);
 
 
-  // Execute a function when the user releases a key on the keyboard
-searchInput.addEventListener("keyup", function(event) {
+// Execute a function when the user releases a key on the keyboard
+searchInput.addEventListener("keyup", function (event) {
   // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
     // Cancel the default action, if needed
@@ -48,22 +48,22 @@ searchInput.addEventListener("keyup", function(event) {
 });
 
 function fetchStatus() {
-let pokeSearch = document.getElementById('search');
-let single = document.getElementById('name');
-let collection = document.getElementById('collection');
-let placeholder = document.getElementById('searchValue');
+  let pokeSearch = document.getElementById('search');
+  let single = document.getElementById('name');
+  let collection = document.getElementById('collection');
+  let placeholder = document.getElementById('searchValue');
 
-  if(single.checked){
-    pokeSearch.removeEventListener('click',catchList);
-    pokeSearch.addEventListener('click',catchSingle);
+  if (single.checked) {
+    pokeSearch.removeEventListener('click', catchList);
+    pokeSearch.addEventListener('click', catchSingle);
     placeholder.placeholder = "Enter Name or ID";
   }
 
-  if(collection.checked){
-    pokeSearch.removeEventListener('click',catchSingle);
-    pokeSearch.addEventListener('click',catchList);
+  if (collection.checked) {
+    pokeSearch.removeEventListener('click', catchSingle);
+    pokeSearch.addEventListener('click', catchList);
     placeholder.placeholder = "Enter Amount";
-  }  
+  }
 }
 
 //Fetching Functions //
@@ -71,50 +71,49 @@ let placeholder = document.getElementById('searchValue');
 //Fetch information for one pokemon
 function catchSingle() {
   var input = document.getElementById("searchValue").value;
-  console.log(input);
   if (input > 0 && input <= 893) {
     var pURL = input;
-    while(prevUrl != pURL) {
+    while (prevUrl != pURL) {
       mainPkCount += 1;
- /////////////////Fetch
- fetch("https://pokeapi.co/api/v2/pokemon/" + input + "/")
-.then(response => {
-  if (!response.ok) {
-    throw new Error("Network response was not okay");
-  }
-  return response.json();
-})
+      /////////////////Fetch
+      fetch("https://pokeapi.co/api/v2/pokemon/" + input + "/")
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Network response was not okay");
+          }
+          return response.json();
+        })
 
-.then((pokemon) => {
-  createSingle(pokemon) ;
-})
+        .then((pokemon) => {
+          createSingle(pokemon);
+        })
 
-.catch((error) => {
-  // Handle the error
-  console.log("should be an error for a 404");
-  console.log(error);
-  alert("error!");
-});
+        .catch((error) => {
+          // Handle the error
+          console.log("should be an error for a 404");
+          console.log(error);
+          alert("error!");
+        });
 
-   
-//////////end fetch
-    pCount.innerHTML = mainPkCount + " Pokemon Fetched";
-    document.getElementById("searchValue").value = "";
-    prevUrl = pURL;
+
+      //////////end fetch
+      pCount.innerHTML = mainPkCount + " Pokemon Fetched";
+      document.getElementById("searchValue").value = "";
+      prevUrl = pURL;
     }
   }
   //if not an interger or empty string
   else if (input != "") {
     var LowerInput = input.toLowerCase(0);
-    while(prevUrl != input) {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + LowerInput + "/")
-      .then((response) => response.json())
-      .then(function(pokemon) {
-        createSingle(pokemon);
-      });
-    document.getElementById("searchValue").value = "";
-    prevUrl = input;
-    mainPkCount += 1;
+    while (prevUrl != input) {
+      fetch("https://pokeapi.co/api/v2/pokemon/" + LowerInput + "/")
+        .then((response) => response.json())
+        .then(function (pokemon) {
+          createSingle(pokemon);
+        });
+      document.getElementById("searchValue").value = "";
+      prevUrl = input;
+      mainPkCount += 1;
     }
   }
   //error handling
@@ -127,16 +126,15 @@ function catchSingle() {
 
 //Fetch Multiple Pokemon
 function catchList() {
-  console.log("catchList was hit");
   var input = document.getElementById("searchValue").value;
   if (input > 0) {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=" + input)
       .then((response) => response.json())
-      .then(function(allPokemon) {
+      .then(function (allPokemon) {
         pCount.innerHTML = input + " Pokemon Fetched!";
-        allPokemon.results.forEach(function(pokemon) {
+        allPokemon.results.forEach(function (pokemon) {
           fetchPokemonData(pokemon);
-          
+
         });
       });
     input.value = "";
@@ -151,11 +149,11 @@ function catchList() {
 //Fetch Random Amount
 function catchRandom() {
   var alg = Math.floor(Math.random(1) * 650); {
-  fetch("https://pokeapi.co/api/v2/pokemon/" + alg)
-    .then((response) => response.json())
-    .then(function(pokemon) {
-      createSingle(pokemon);
-    });
+    fetch("https://pokeapi.co/api/v2/pokemon/" + alg)
+      .then((response) => response.json())
+      .then(function (pokemon) {
+        createSingle(pokemon);
+      });
   }
 }
 
@@ -163,7 +161,7 @@ function fetchPokemonData(pokemon) {
   let url = pokemon.url;
   fetch(url)
     .then((response) => response.json())
-    .then(function(pokeData) {
+    .then(function (pokeData) {
       renderPokemon(pokeData);
     });
 }
@@ -179,7 +177,7 @@ function renderPokemon(pokeData) {
 
   let container = document.getElementById("test");
   let pokeContainer = document.createElement("div");
-  pokeContainer.classList.add("col-m-6", "m-4","pb-4", "align-content-center", "success");
+  pokeContainer.classList.add("col-m-6", "m-4", "pb-4", "align-content-center", "success");
   container.classList.add("ui");
   var pokeImage = document.createElement("img");
   var pokeName = document.createElement("h1");
@@ -212,18 +210,23 @@ function renderPokemon(pokeData) {
     pokeWeight,
     baseStats
   );
-  container.prepend(pokeContainer);
+
+  let colorType = pokeData.types[0].type.name;
+  setColor(colorType, pokeContainer);
+
+  container.append(pokeContainer);
 } //end multi
 
 //Render Single Pokemon
 function createSingle(pokeData) {
+
   var heightMath = (pokeData.height / 3.048).toFixed(2);
   var weightMath = (pokeData.weight / 4.536).toFixed(2);
   var baseStat = pokeData.base_experience;
 
   let container = document.getElementById("test");
   let pokeContainer = document.createElement("div");
-  pokeContainer.classList.add("col-m-6", "card", "m-4", "pb-4" ,"align-content-center");
+  pokeContainer.classList.add("col-m-6", "card", "m-4", "pb-4", "align-content-center");
   var pokeImage = document.createElement("img");
   var pokeName = document.createElement("h1");
   var pokeId = document.createElement("p");
@@ -248,6 +251,7 @@ function createSingle(pokeData) {
   pokeName.innerHTML = pokeData.name;
   pokeId.innerHTML = `#${pokeData.id}`;
   poketypes.innerHTML = "Types: " + pokeData.types.map((el) => el.type.name);
+  // checkTypes();
   pokeHeight.innerHTML = "Height: " + heightMath + " Feet";
   pokeWeight.innerHTML = "Weight: " + weightMath + " lbs";
   baseStats.innerHTML = "EXP earned: " + baseStat;
@@ -261,6 +265,9 @@ function createSingle(pokeData) {
     pokeWeight,
     baseStats
   );
+
+  let colorType = pokeData.types[0].type.name;
+  setColor(colorType, pokeContainer);
   container.prepend(pokeContainer);
 
 } // end single catch -- Should be able to combine single and multi, pass in a paramter to differ between the two
@@ -268,11 +275,74 @@ function createSingle(pokeData) {
 //Misc Functions//
 //****************************************************************************************************************************** */
 
+//Sets color of pokemon card based on type
+function setColor(color, pokeContainer) {
+  let x = color
+  switch (x) {
+    case "normal":
+      pokeContainer.style.backgroundColor = "#A8A77A";
+      break;
+    case "fire":
+      pokeContainer.style.backgroundColor = "#EE8130";
+      break;
+    case "water":
+      pokeContainer.style.backgroundColor = "#6390F0";
+      break;
+    case "electric":
+      pokeContainer.style.backgroundColor = "#F7D02C";
+      break;
+    case "grass":
+      pokeContainer.style.backgroundColor = "#7AC74C";
+      break;
+    case "ice":
+      pokeContainer.style.backgroundColor = "#96D9D6";
+      break;
+    case "fighting":
+      pokeContainer.style.backgroundColor = "#C22E28";
+      break;
+    case "poison":
+      pokeContainer.style.backgroundColor = "#A33EA1";
+      break;
+    case "ground":
+      pokeContainer.style.backgroundColor = "#E2BF65";
+      break;
+    case "flying":
+      pokeContainer.style.backgroundColor = "#A98FF3";
+      break;
+    case "psychic":
+      pokeContainer.style.backgroundColor = "#F95587";
+      break;
+    case "bug":
+      pokeContainer.style.backgroundColor = "#A6B91A";
+      break;
+    case "rock":
+      pokeContainer.style.backgroundColor = "#B6A136";
+      break;
+    case "ghost":
+      pokeContainer.style.backgroundColor = "#735797";
+      break;
+    case "dragon":
+      pokeContainer.style.backgroundColor = "#6F35FC";
+      break;
+    case "dark":
+      pokeContainer.style.backgroundColor = "#705746";
+      break;
+    case "steel":
+      pokeContainer.style.backgroundColor = "#B7B7CE";
+      break;
+    case "fairy":
+      pokeContainer.style.backgroundColor = "#D685AD";
+      break;
+    default:
+      pokeContainer.style.backgroundColor = "ghostwhite";
+  }
+}
+
 //Clear out pokemon display and reset values
 function clearList() {
   document.getElementById("test").innerHTML = "";
   document.getElementById("searchValue").value = "";
-  document.getElementById("catchOne").disabled = false;  
+  document.getElementById("catchOne").disabled = false;
   pCount.innerHTML = "";
   counter = 1;
   mainPkCount = 0;
@@ -280,39 +350,39 @@ function clearList() {
 
 //Invert colors (Should take in a couple parameters..for some type of functionallity)
 function invertColors() {
-//navigation bar
-document.getElementById('header').style.backgroundColor="black";
-document.getElementById('title').style.color="white";
-document.getElementById('title').style.backgroundcolor="black";
-document.getElementById('menu').style.backgroundColor="black";
-document.getElementById('background').style.backgroundColor="black";
-//change display pokemon count color
-document.getElementById("displayCount").style.backgroundColor ="black";
-pCount.style.color = "white";
-//container that pokemon are held on
-document.getElementById("content").style.backgroundColor = "black";
-document.getElementById("test").style.backgroundColor = "black";
+  //navigation bar
+  document.getElementById('header').style.backgroundColor = "black";
+  document.getElementById('title').style.color = "white";
+  document.getElementById('title').style.backgroundcolor = "black";
+  document.getElementById('menu').style.backgroundColor = "black";
+  document.getElementById('background').style.backgroundColor = "black";
+  //change display pokemon count color
+  document.getElementById("displayCount").style.backgroundColor = "black";
+  pCount.style.color = "white";
+  //container that pokemon are held on
+  document.getElementById("content").style.backgroundColor = "black";
+  document.getElementById("test").style.backgroundColor = "black";
 }
 
 //Invert back to normal UI
 function revertColors() {
   //navigation bar
-document.getElementById('header').style.backgroundColor="white";
-document.getElementById('title').style.color="black";
-document.getElementById('title').style.backgroundcolor="white";
-document.getElementById('menu').style.backgroundColor="white";
-document.getElementById('background').style.backgroundColor="white";
-//change display pokemon count color
-document.getElementById("displayCount").style.backgroundColor ="white";
-pCount.style.color = "black";
-//container that pokemon are held on
-document.getElementById("content").style.backgroundColor = "white";
-document.getElementById("test").style.backgroundColor = "white";
-document.getElementById("footer").style.backgroundColor = "white";
-document.getElementById('footerText').style.color = "black";
+  document.getElementById('header').style.backgroundColor = "white";
+  document.getElementById('title').style.color = "black";
+  document.getElementById('title').style.backgroundcolor = "white";
+  document.getElementById('menu').style.backgroundColor = "white";
+  document.getElementById('background').style.backgroundColor = "white";
+  //change display pokemon count color
+  document.getElementById("displayCount").style.backgroundColor = "white";
+  pCount.style.color = "black";
+  //container that pokemon are held on
+  document.getElementById("content").style.backgroundColor = "white";
+  document.getElementById("test").style.backgroundColor = "white";
+  document.getElementById("footer").style.backgroundColor = "white";
+  document.getElementById('footerText').style.color = "black";
 }
 
 //Accordion 
 $('.ui.accordion')
   .accordion()
-;
+  ;
