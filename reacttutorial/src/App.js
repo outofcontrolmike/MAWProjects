56aca28aba09b239533f9332bdc8ffad86c70fac
Michael Wilson <mike@beardeddevops.com>
2021-09-01 15:48:18 -0500

    started to do list

--- reacttutorial/src/App.js
+++ reacttutorial/src/App.js
@@ -1,9 +1,13 @@
-
-
 function App() {
   return (
     <div>
-      Howdy Stranger
+      <h1>My Todos</h1>
+      <div>
+        <h2>Title</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
