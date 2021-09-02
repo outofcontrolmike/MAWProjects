2070560bfd6a9f4d749c8501997ea88589e0eabc
Michael Wilson <mike@beardeddevops.com>
2021-09-02 15:54:36 -0500

    went over listing mettings

--- meetUP/src/App.js
+++ meetUP/src/App.js
@@ -2,11 +2,13 @@ import {Route, Switch} from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     //localhost:3000
     <div>
+      <MainNavigation />
       <Switch>
     <Route path='/' exact={true}>
       <AllMeetupsPage />
