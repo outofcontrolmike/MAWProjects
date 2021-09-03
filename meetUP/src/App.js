be7569e112199e15e9004e706ec90a19b8261b00
Michael Wilson <mike@beardeddevops.com>
2021-09-03 15:13:35 -0500

    worked on styling

--- meetUP/src/App.js
+++ meetUP/src/App.js
@@ -2,13 +2,12 @@ import {Route, Switch} from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout  from './components/layout/Layout';
 
 function App() {
   return (
     //localhost:3000
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
     <Route path='/' exact={true}>
       <AllMeetupsPage />
@@ -20,7 +19,7 @@ function App() {
       <FavoritesPage/>
     </Route>
     </Switch>
-    </div>
+    </Layout>
   );
 }
 
--- meetUP/src/components/meetups/MeetupItem.js
+++ meetUP/src/components/meetups/MeetupItem.js
@@ -1,8 +1,10 @@
 import classes from './MeetupItem.module.css';
+import Card from "../ui/Card";
 
 function MeetupItem(props) {
     return(
         <li className={classes.item}>
+            <Card>
             <div className={classes.image}>
                 <img src={props.image} alt={props.title} />
             </div>
@@ -14,6 +16,7 @@ function MeetupItem(props) {
             <div className={classes.action}>
             <button>TO Favorites</button>
             </div>
+            </Card>
         </li>
     )
 
