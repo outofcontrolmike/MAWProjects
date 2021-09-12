9a37b8bc5c6257133b42ff55c96171086dba2ca9
Michael Wilson <outofcontrolmike@gmail.com>
2021-09-12 16:50:21 -0500

    added favorites badge to main navigation component

--- characters/src/components/layout/MainNavigation.js
+++ characters/src/components/layout/MainNavigation.js
@@ -1,6 +1,10 @@
 import {Link} from 'react-router-dom';
+import {useContext} from 'react';
+import FavoritesContext from '../../store/favorites-context';
 
 function MainNavigation() {
+    const favoritesCtx = useContext(FavoritesContext);
+
 return <header>
     <div>
         Characters
@@ -14,7 +18,8 @@ return <header>
             <Link to ='/new-character'>New Character</Link>
             </li>
             <li>
-            <Link to ='/favorites'>Favorite Characters</Link>
+            <Link to ='/favorites'>Favorite Characters
+            <span>{favoritesCtx.totalFavorites}</span></Link>
             </li>
             <li>
                 <button>Help</button>
