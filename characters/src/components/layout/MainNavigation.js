0bc60c1b91f1b907fbee423e9ad733954dcf583f
Michael Wilson <mike@beardeddevops.com>
2021-09-15 10:02:08 -0500

    nav style

--- characters/src/components/layout/MainNavigation.js
+++ characters/src/components/layout/MainNavigation.js
@@ -6,10 +6,10 @@ function MainNavigation() {
     const favoritesCtx = useContext(FavoritesContext);
 
 return <header style={{ marginBottom:"2rem"}}>
-<div className="ui menu four item secondary pointing">
+<div className="ui menu four item pointing secondary">
     <div className="ui container">
 
-      <a className="item"><Link to ='/'>All Characters</Link></a>
+      <a className="item active"><Link to ='/'>All Characters</Link></a>
       <a className="item"><Link to ='/new-character'>New Character</Link></a>
       <a className="item"><Link to ='/favorites'>Favorites <span>{favoritesCtx.totalFavorites}</span></Link></a>
       <a className="item" tabindex="0">
