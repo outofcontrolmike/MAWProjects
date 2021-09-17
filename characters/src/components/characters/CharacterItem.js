b853e7216cf8aa9f9735175d7846f1279e148e7a
Michael Wilson <mike@beardeddevops.com>
2021-09-17 15:04:31 -0500

    cards are formated

--- characters/src/components/characters/CharacterItem.js
+++ characters/src/components/characters/CharacterItem.js
@@ -35,15 +35,16 @@ function CharacterItem(props) {
             <div className="content">
                 <div className="header">{props.name}</div>
                 <div className="ui meta">
-                    <div>{props.origin}</div>
-                    <div>{props.gender}, {props.age} years of age</div>
+                    <div>Origin: {props.origin}</div>
                 </div>
+                
                 <div class="description">
+                    <p style={{marginBottom: "1rem"}}>{props.age} year old {props.gender} </p>
                     {props.description}
                 </div>
             </div>
             <div class="extra content">
-            <span id="favoriteBtn" onClick={toggleFavoriteStatus}>{itemIsFavorite ? <button className="ui button inverted red small">Remove From Favorites</button> : <button className="ui button inverted blue small">Add To Fravorites</button>}</span>
+            <span id="favoriteBtn" onClick={toggleFavoriteStatus}>{itemIsFavorite ? <button className="ui button fluid red small">Remove From Favorites</button> : <button className="ui button blue small fluid">Add To Fravorites</button>}</span>
 
             </div>
        
