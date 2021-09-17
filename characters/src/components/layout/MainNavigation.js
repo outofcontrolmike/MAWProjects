ed0e40ce6e8f78816d396357d9af96ff616344eb
Michael Wilson <mike@beardeddevops.com>
2021-09-17 14:20:14 -0500

    implemented character total

--- characters/src/components/layout/MainNavigation.js
+++ characters/src/components/layout/MainNavigation.js
@@ -1,71 +1,105 @@
-import {Link} from 'react-router-dom';
-import {useContext} from 'react';
-import FavoritesContext from '../../store/favorites-context';
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
+function MainNavigation(props) {
+  const favoritesCtx = useContext(FavoritesContext);
 
-function MainNavigation() {
-    const favoritesCtx = useContext(FavoritesContext);
+  let help = document.getElementById("help");
 
-    let help = document.getElementById('help');
+  let length = 0;
+  grabCharacterTotal(length);
 
-      let cards = document.getElementsByClassName('card');
-
-      let counter = 0;    
-      counter = cards.length;
-
-   function handleAllCharacters() {
-    const allCharacters = document.getElementById('allCharacters');
-    const newCharacter = document.getElementById('newCharacter');
-    const favorites = document.getElementById('favorites');
-
-        newCharacter.className = 'item';
-        favorites.className = 'item';
-        allCharacters.className = 'item active';     
+  function grabCharacterTotal() {
+    {
+      fetch(
+        "https://characters-ba8da-default-rtdb.firebaseio.com/characters.json"
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          objectLength(data);
+        });
     }
+  }
+
+  function objectLength(data) {
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        ++length;
+      }
+    }
+    console.log(length);
+    document.getElementById("length").innerHTML = length;
+    document.getElementById("characterTotal").innerHTML = length;
+    return length;
+  }
+
+  function handleAllCharacters() {
+    const allCharacters = document.getElementById("allCharacters");
+    const newCharacter = document.getElementById("newCharacter");
+    const favorites = document.getElementById("favorites");
+
+    newCharacter.className = "item";
+    favorites.className = "item";
+    allCharacters.className = "item active";
+  }
 
   function handleNewCharacter() {
-    const allCharacters = document.getElementById('allCharacters');
-    const newCharacter = document.getElementById('newCharacter');
-    const favorites = document.getElementById('favorites');
+    const allCharacters = document.getElementById("allCharacters");
+    const newCharacter = document.getElementById("newCharacter");
+    const favorites = document.getElementById("favorites");
 
-    allCharacters.className = 'item';
-    favorites.className = 'item';
-    newCharacter.className = 'item active';
-    
+    allCharacters.className = "item";
+    favorites.className = "item";
+    newCharacter.className = "item active";
   }
 
   function handleFavorite() {
-    const allCharacters = document.getElementById('allCharacters');
-    const newCharacter = document.getElementById('newCharacter');
-    const favorites = document.getElementById('favorites');
+    const allCharacters = document.getElementById("allCharacters");
+    const newCharacter = document.getElementById("newCharacter");
+    const favorites = document.getElementById("favorites");
 
-    allCharacters.className = 'item';
-    newCharacter.className = 'item';
-    favorites.className = 'item active';
+    allCharacters.className = "item";
+    newCharacter.className = "item";
+    favorites.className = "item active";
   }
 
   //help modal
 
-return <header style={{ marginBottom:"2rem"}}>
-<div className="ui menu five item secondary pointing" id="mainNavigation">
-    <div className="ui container">
-
-      <a className="item active" id="allCharacters" onClick={handleAllCharacters}><Link to ='/'>All Characters <span>{counter}</span></Link></a>
-      <a className="item" id="newCharacter" onClick={handleNewCharacter}><Link to ='/new-character'>New Character</Link></a>
-      <a className="item"  id="favorites" onClick={handleFavorite}><Link to ='/favorites'>Favorites <span>{favoritesCtx.totalFavorites}</span></Link></a>
-      <a className="item" id="help" tabindex="0">
-        <Link to = "/">Help</Link>
-      </a>
-      <div className="item ui statistic">
-  <div class="value">
-    {counter}
-  </div>
-  <div>
-    Characters
-  </div>
-</div>
-    </div>
-  </div>
-</header>
+  return (
+    <header style={{ marginBottom: "2rem" }}>
+      <div className="ui menu five item secondary pointing" id="mainNavigation">
+        <div className="ui container">
+          <a
+            className="item active"
+            id="allCharacters"
+            onClick={handleAllCharacters}
+          >
+            <Link to="/">
+              All Characters <span id="length"></span>
+            </Link>
+          </a>
+          <a className="item" id="newCharacter" onClick={handleNewCharacter}>
+            <Link to="/new-character">New Character</Link>
+          </a>
+          <a className="item" id="favorites" onClick={handleFavorite}>
+            <Link to="/favorites">
+              Favorites <span>{favoritesCtx.totalFavorites}</span>
+            </Link>
+          </a>
+          <a className="item" id="help" tabindex="0">
+            <Link to="/">Help</Link>
+          </a>
+          <div className="item ui statistic">
+            <div class="value" id="characterTotal"></div>
+            <div>Characters</div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default MainNavigation;
