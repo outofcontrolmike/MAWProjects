41079c8400f03dc5903103877dca8308ff9f2948
Michael Wilson <mike@beardeddevops.com>
2021-09-22 10:28:37 -0500

    winner functionallity is detmerined for tic tac toe

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -31,7 +31,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player:' + (this.state.xIsNext ? 'X' : "O");
+    let status; 
+    const winner = calculateWinner(this.state.squares);
+    if(winner) {
+      status = "Winner : " + winner;
+    }
+    else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
@@ -78,4 +85,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
