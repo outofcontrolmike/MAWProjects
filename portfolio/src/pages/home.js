2d25d3ca50f93e6e95e50005e49f90309cc51778
Michael Wilson <outofcontrolmike@gmail.com>
2021-09-23 18:19:43 -0500

    Landing page rough draft

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -1,10 +1,12 @@
-import picture from '../assets/img/landingPage.jpg';
+import picture from '../assets/img/Yosemite.jpg';
 
 function Home() {
     return(
-  <div class="column">
-        <img src={picture} className="ui image fluid" id="landingpageImage"></img>
-  </div>
+        <div id="overlay">
+            <img className="ui image fluid" id="landingPage" src={picture}/>
+        <h1 id="landingPageText">Developing software is similar to adventuring. Sometimes you get lost, but you make it out stronger and alive.</h1>
+        </div>
+    
     )
 }
 
