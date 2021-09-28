afb8e97f70bc32f4198c918e5504f80be69ed656
Michael Wilson <outofcontrolmike@gmail.com>
2021-09-27 21:17:35 -0500

    most of about is done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom'
+import {Link, Route, Switch} from 'react-router-dom'
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from "./pages/Home";
--- portfolio/src/pages/about.js
+++ portfolio/src/pages/about.js
@@ -1,5 +1,5 @@
 import Navigation from "../components/layout/Navigation";
-import myPicture from "../assets/img/myImage.jpg";
+import myPicture from "../assets/img/profilePic.jpg";
 import secondPicture from "../assets/img/tacos.jpg";
 
 import htmlCert from "../assets/img/html.png";
@@ -12,30 +12,17 @@ function About() {
   return (
     <>
       <Navigation />
-      <div className="ui " id="about">
-        <div className="ui two column doubling stackable grid padded" id="about" >
-          <div className="ui column">
-          <div className="ui card centered" style={{width: "fit-content"}}>
-                <div className="ui slide masked reveal image">
-                  <img src={myPicture} className="visible content" style={{objectFit: "cover", height: "700px"}}/>
-                  <img src={secondPicture} className="hidden content fluid" style={{objectFit: "cover", height: "400px"}}/>
-                </div>
-                <div className="content">
-                  <a className="header">Michael Wilson</a>
-                  <div className="meta">
-                    <span className="date">Graduated Fall 2019</span>
-                    <p className="date">Associates of Applied Science Computer Information Science</p>
-                  </div>
-                </div>
-              </div>
+        <div className="ui two column doubling stackable grid padded segment" id="about" >
+          <div className="ui column" id="pictureContainer">
+          <img className= "ui image centered big rounded"  style={{}}src={myPicture} />
           </div>
-          <div className="ui column">
+          <div className="ui column padded">
               <h1>Who am I?</h1>
               <hr></hr>          
             <p>
-              <span className="ui text large">Michael Wilson</span><br></br><br></br>
+              <span className="ui text large">Mr. Michael Wilson</span><br></br><br></br>
 
-              <p>I have been involved with WebDevelopment and programming since 2018.</p>
+              <p>I am a person that is always trying to learn new skills and enjoy the process at the same time. I belive there is nothing better than working on a passion project. I have been involved with Web Development and Programming since the Spring of 2018.</p>
             </p>
             <p>
               I originally gained interest for Web development during a previous job I had, managing an online vitamin store through woo commerce.  I found it interesting to use tools through wordpress to alter the front end of my company's website.
@@ -44,18 +31,16 @@ function About() {
             <p>
             <h1>What I Do</h1>
             <hr></hr>
-              Currently I work with react Js in a proffesional sense. During my work hours I fix bugs and implement feature requests among a few of my compaines projects built with the react library<span>REACT Sybmol**</span>
+              Currently I work with the Javascript coding language, Specifically in the <span> <a target="_blank" href="www.google.com">React JS Library.</a></span> My tasks include fixing bugs and implementing feature requests among a few of my compaines projects built with the react library.
             </p>
-            <p>With some of my free time I try to build projects with React so I keep up to date with work.</p>
+            <p>With some of my free time I try to build projects with React so I keep up to date with work.  I generally like to mess around with API's.</p>
             <h1>Besides Coding</h1>
             <hr></hr>
             <p>
-              I really love hiking and traveling. I also enjoy photography and writing. I try to be with family and friends as much as I can since conversations with them can spark ideas for new projects to work on.
-
+              I really love hiking, travelilng and trying a vareity of new things. I also enjoy Guitar, Writing and Photography.  I of course work on my over 100 year old house and keep learning new things about being a homeowner.
             </p>
           </div>
         </div>
-      </div>
 
     </>
   );
