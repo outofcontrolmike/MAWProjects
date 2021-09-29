b463e959b4e9b83b6da8ed6621d6aa845e0af761
Michael Wilson <outofcontrolmike@gmail.com>
2021-09-29 18:29:03 -0500

    some changes to Resume page

--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -3,16 +3,41 @@ function Resume() {
     return(
         <div>
         <Navigation />
-        <div className="ui three column segment piled stackable grid">
-            <div className="ui column">
-                <h1>Resume</h1>
-                
-            </div>
+        <div className="ui two column relaxed very padded segment piled stackable grid">
             <div className="ui column">
                 <h1>Skills</h1>
+                <hr></hr>
+                <div>
+                    <ul>
+                        <li>HTML</li>
+                        <li>Word Processing & Excel</li>
+                        <li>Windows</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>CSS</li>
+                        <li>Javascript</li>
+                        <li>PHP</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>C#</li>
+                        <li>Java</li>
+                        <li>SQL Server</li>
+                        <li>XML</li>
+                        <li>Unbuntu</li>
+                        <li>Command Line</li>
+                    </ul>
+                </div>
             </div>
             <div className="ui column">
-                <h1>Certs and misc</h1>
+                <h1>Certifications & Achievments</h1>
+                <hr></hr>
+                <h1>HTML & CSS</h1>
+                <h1>JavaScript</h1>
+                <h1>SQL</h1>
             </div>
         </div>
         </div>
