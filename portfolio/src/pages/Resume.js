351a9467396512cac124621d0c7ddfc2c0a6d06d
Michael Wilson <outofcontrolmike@gmail.com>
2021-09-28 21:28:54 -0500

    generic layout for resume page

--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -3,7 +3,18 @@ function Resume() {
     return(
         <div>
         <Navigation />
-        <div>Testing</div>
+        <div className="ui three column segment piled stackable grid">
+            <div className="ui column">
+                <h1>Resume</h1>
+                
+            </div>
+            <div className="ui column">
+                <h1>Skills</h1>
+            </div>
+            <div className="ui column">
+                <h1>Certs and misc</h1>
+            </div>
+        </div>
         </div>
 
     )
