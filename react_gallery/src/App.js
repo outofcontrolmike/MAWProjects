6ccb7a5a8dcc2603f7fcc827a5ebec9f714b0602
Michael Wilson <mike@beardeddevops.com>
2021-10-15 15:46:09 -0500

    wip: react basics, tailwind css

--- react_gallery/src/App.js
+++ react_gallery/src/App.js
@@ -1,6 +1,20 @@
+import React from "react";
+import "./assets/css/styles.css";
+
 function App() {
-  let el = <div>Hello React</div>;
-  return el;
+  return <div className="box">Hello from functional component</div>;
 }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { title: "Hello React from the state" };
+//   }
+
+//   render() {
+//     let el = <div>{this.state.title}</div>;
+//     return el;
+//   }
+// }
 
 export default App;
