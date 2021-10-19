46ad41c67243921bc4c62920e4324e1a6a52a45c
Michael Wilson <mike@beardeddevops.com>
2021-10-19 08:19:30 -0500

    test: making sure new changes work to stying

--- react_gallery/src/App.js
+++ react_gallery/src/App.js
@@ -1,35 +1,34 @@
 import React from "react";
 import "./assets/css/style.css";
-import Images from "../src/components/images"
+import Images from "../src/components/images";
 
 class App extends React.Component {
-
   constructor(props) {
-    super(props)
-    this.state = { title: "Hello React 2!!", isShowing: false}
+    super(props);
+    this.state = { title: "Hello React 2!!", isShowing: false };
   }
 
   handleClick = () => {
-    this.setState({ isShowing: !this.state.isShowing})
-  }
+    this.setState({ isShowing: !this.state.isShowing });
+  };
 
   render() {
-  return(
-    <section className="flex justify-center">
-      <div className="w-1/2">
-        <div className="text-center">
-          <div className="my-4">{this.state.title}</div>
-          <button className="p-10 bg-blue-700 text-white my-2"
-                  onClick={this.handleClick}>
-          
-          Toggle Image</button>
+    return (
+      <section className="flex justify-center">
+        <div className="w-1/2">
+          <div className="text-center">
+            <div className="my-4">{this.state.title}</div>
+            <button
+              className="p-10 bg-blue-700 rounded-md text-white my-2"
+              onClick={this.handleClick}
+            >
+              Toggle Image
+            </button>
           </div>
-          {this.state.isShowing ? (
-            <Images />
-          ) : null}
-      </div>
-    </section>
-  )
+          {this.state.isShowing ? <Images /> : null}
+        </div>
+      </section>
+    );
   }
 }
 
