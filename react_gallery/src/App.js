f918ec66d08137bf663b99d7e1a306d946e14ebc
Michael Wilson <outofcontrolmike@gmail.com>
2021-10-18 20:55:55 -0500

    image is toggleable by using state

--- react_gallery/src/App.js
+++ react_gallery/src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
 import "./assets/css/style.css";
+import Images from "../src/components/images"
 
-function App({ title }) {
-  return <div className="bg-gray-600 text-white p-10">{title}</div>;
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { title: "Hello React 2!!", isShowing: false}
+  }
+
+  handleClick = () => {
+    this.setState({ isShowing: !this.state.isShowing})
+  }
+
+  render() {
+  return(
+    <section className="flex justify-center">
+      <div className="w-1/2">
+        <div className="text-center">
+          <div className="my-4">{this.state.title}</div>
+          <button className="p-10 bg-blue-700 text-white my-2"
+                  onClick={this.handleClick}>
+          
+          Toggle Image</button>
+          </div>
+          {this.state.isShowing ? (
+            <Images />
+          ) : null}
+      </div>
+    </section>
+  )
+  }
 }
+
+// function App({ title }) {
+//   return <div className="bg-gray-600 text-white p-10">{title}</div>;
+// }
 
 export default App;
---
+++ react_gallery/src/components/images.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class images extends Component {
+    render() {
+        return (
+            <div>
+                <img  alt="first photography" src="https://images.unsplash.com/photo-1634549667619-bbbdb4b098a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80" />
+
+            </div>
+        )
+    }
+}
