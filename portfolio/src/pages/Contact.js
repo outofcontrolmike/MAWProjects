15cb675ed26364b73efbd8576eef96d2a4b8a075
Michael Wilson <outofcontrolmike@gmail.com>
2021-11-06 15:48:52 -0500

    wip: contact page

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,11 +1,41 @@
 import Navigation from "../components/layout/Navigation"
 
-export default function Contact() {
-        return(
-            
-            <div>
-                      <Navigation />
+//Contact page requirements - > Email form (name,email, category selection, message)
 
+//Also place in a section with contact links (phone number, email address, social icons and links)
+export default function Contact() {
+    return (
+
+        <div>
+            <Navigation />
+            <div className="ui column stackable very padded relaxed container">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Full Name</label>
+                        <input id="name" type="text" name="name" placeholder="Full Name" />
+                    </div>
+                    <div className="field">
+                        <label>Email Address</label>
+                        <input id="email" type="text" name="email" placeholder="Prefered Email Address" />
+                    </div>
+                    <div className="field">
+                        <label>Category</label>
+                        <select id="category" className="ui fluid dropdown">
+                            <option value="">Category</option>
+                            <option>General</option>
+                            <option>Suggestion</option>
+                            <option>Complaint</option>
+                            <option>Compliment</option>
+                        </select>
+                    </div>
+                    <div className="field">
+                        <label>Message</label>
+                        <textarea id="message" type="text" name="message" placeholder="Message" />
+                    </div>
+                    <button className="ui button orange" id="submit" type="submit">Submit</button>
+                </form>
             </div>
-        )
-    }
+
+        </div>
+    )
+}
--- portfolio/src/pages/about.js
+++ portfolio/src/pages/about.js
@@ -66,7 +66,7 @@ function About() {
           </p>
         </div>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
