1eec7614f46385211eac05cf54044ad450aa5050
Michael Wilson <outofcontrolmike@gmail.com>
2021-11-07 12:21:49 -0600

    thanks page almost fully functional

--- portfolio/src/pages/Thanks.js
+++ portfolio/src/pages/Thanks.js
@@ -1,7 +1,19 @@
 function Thanks() {
+
+function handleClick()
+{
+    console.log("button KNows it's being clicked")
+    window.open("http://localhost:3000/");
+}
+
     return(
-        <div>
-            <p style={{color: "white"}}>Thank you!</p></div>
+        <div className="container ui relaxed padded">
+            <h1>Thank you so much, {"userName"}!!</h1>
+            <h3>I will be getting back to you as soon as I can!</h3>
+            <div>
+  <button className="ui inverted blue button huge" onClick={handleClick}>Back to Home</button>
+            </div>
+            </div>
     )
 }
 
