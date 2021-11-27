378dad4ba46d07b0dbe89e89a7f6f7a9e2bc6bbd
Michael Wilson <mike@beardeddevops.com>
2021-11-27 15:05:10 -0600

    fix: brewFooter mobile responsivness and normal positioning

--- portfolio/src/pages/projects/breweries/Brewery.js
+++ portfolio/src/pages/projects/breweries/Brewery.js
@@ -1,19 +1,18 @@
 import BrewMenu from "../../../components/brewereyFetch/BrewMenu";
 import BrewModal from "../../../components/brewereyFetch/BrewModal";
-import $ from 'jquery';
 import BrewFooter from "../../../components/brewereyFetch/BrewFooter";
 
 export default function Brewery() {
-
-    console.log('You are running jQuery version: ' + $.fn.jquery)
-    return (
-        <body className="ui segment" style={{ backgroundColor: "black", height: "100vh" }}>
-            <BrewMenu />
-            <div id="breweries" className="ui" style={{ marginTop: "50px" }}></div>
-            <BrewFooter />
-            {/* Brew Modal */}
-            <BrewModal />
-
-        </body>
-    );
+  return (
+    <body
+      className="ui segment"
+      style={{ backgroundColor: "black", height: "100vh" }}
+    >
+      <BrewMenu />
+      <div id="breweries" className="ui" style={{ marginTop: "50px" }}></div>
+      <BrewFooter />
+      {/* Brew Modal */}
+      <BrewModal />
+    </body>
+  );
 }
