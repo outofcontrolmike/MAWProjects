0dd9660839a0fef7d09795ed9ae432f697e779d5
Michael Wilson <outofcontrolmike@gmail.com>
2021-12-02 20:37:35 -0600

    wip: intergrating js into pokmeon react conversion

--- portfolio/src/pages/projects/pokemon/Pokemon.js
+++ portfolio/src/pages/projects/pokemon/Pokemon.js
@@ -5,6 +5,9 @@ export default function Pokemon() {
     return(
         <div id="pBackground">
             <PokemonMenu />
+            <div className="row text-center justify-content-center mb-4 ml-4 mr-4" id="test">
+                
+            </div>
         </div>
     )
 }
