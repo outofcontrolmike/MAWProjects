ad06ec5841e0180f675e5774dea861f9f81bc558
Michael Wilson <outofcontrolmike@gmail.com>
2021-12-14 21:08:16 -0600

    style:  Bmi calculator project

--- portfolio/src/components/bmiCalc/BMI.js
+++ portfolio/src/components/bmiCalc/BMI.js
@@ -6,20 +6,23 @@ export default function BMI() {
         <div className='ui'>
             <div id="normalCards" className='ui raised very padded text segment'>
                 <div className="ui large form">
+                <h1 style={{color: "black"}} >Body Mass Index Calculator</h1>
+                <hr/>
+
                     <div className="three fields">
                         <div className="field">
                             <label>Height (in)</label>
-                            <input id="bmiHeight" placeholder="Height" type="tel" />
+                            <input id="bmiHeight" placeholder="Height" type="tel" maxlength="2" maxlength="2" />
                         </div>
                         <div className="field">
                             <label>Weight (lbs)</label>
-                            <input id="bmiWeight" placeholder="Weight" type="text" />
+                            <input id="bmiWeight" placeholder="Weight" type="tel" maxlength="3" max={3} />
                         </div>
                         <div className="field">
-                            <h3 id="bmiInfo">summed value</h3>
+                            <h3 id="bmiInfo"></h3>
                         </div>
                     </div>
-                    <span className="ui"> <button className="ui button" style={{ width: "50%" }} id="bmiCalc">Calculate BMI</button>
+                    <span className="ui"> <button className="ui button" style={{ width: "66%" }} id="bmiCalc">Calculate BMI</button>
                     </span>                </div>
             </div>
 
--- portfolio/src/components/bmiCalc/BMR.js
+++ portfolio/src/components/bmiCalc/BMR.js
@@ -5,41 +5,23 @@ import React from 'react'
 export default function BMR() {
   return (
     <div>
-      <div id="normalCards" className="ui raised very padded text segment">
-        <h1 style={{ color: "black" }}>Basil Metabolic Rate Calculation</h1>
-        <hr />
-        <div className="ui two column doubling stackable grid container">
-          <div className="ui column grid">
-            <label id="bmiLabel">Height (in)</label>
-            <input id="bmrHeight" type="tel" maxlength="2" className="input" />
-            <div> <label id="bmiLabel">Weight(lbs)</label>
-              <input type="tel" id="bmrWeight" maxlength="3" max="3" /></div>
-
-            <label id="bmiLabel">Age</label>
-            <input type="tel" id="bmrAge" maxlength="3" max="110" />
-           
-            <button id="bmrCalc" className="ui button">Calculate BMR</button>
-          </div>
-
-        </div>
-      </div>
-
-      {/* conversion */}
-
       <div id="normalCards" className='ui raised very padded text segment'>
         <div className="ui large form">
+        <h1 style={{color: "black"}}> Basil Metabolic Rate Calculator</h1>
+        <hr/>
+
           <div className="three fields">
             <div className="field">
               <label>Height (in)</label>
-              <input id="bmiHeight" placeholder="Height" type="tel" />
+              <input id="bmrHeight" placeholder="Height" type="tel" maxlength="2" />
             </div>
             <div className="field">
               <label>Weight (lbs)</label>
-              <input id="bmiWeight" placeholder="Weight" type="text" />
+              <input id="bmrWeight" placeholder="Weight" type="text" maxLength="3" />
             </div>
             <div className="field">
               <label id="bmi">Age</label>
-              <input type="tel" id="" maxlength="3" max="110" placeholder='Age' />                        </div>
+              <input type="tel" maxlength="2" id="" maxlength="3" max="110" placeholder='Age' id="bmrAge" />                        </div>
           </div>
           <div className='two fields'>
             <div className="field">
@@ -50,14 +32,13 @@ export default function BMR() {
               </select></div>
             </div>
             <div className="field">
-            <span className="ui"> <button className="ui button" id="bmiCalc">Calculate BMI</button></span>
-
+            <span className="ui"> <button className="ui button" id="bmrCalc" style={{marginTop: "1.8rem"}} >Calculate BMI</button></span>
             </div>
           </div>
           <div className='field'>
           <div className="ui column " id="bmrInfo">
                 <h3>
-                  *Fill out this form to get your BMR
+               
                 </h3>
               </div>
           </div>
