cfe2198762249ac88018a34ea0e39206f8121e37
Michael Wilson <mike@beardeddevops.com>
2021-12-17 11:32:48 -0600

    style: Contact Page

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,45 +1,83 @@
-import Navigation from "../components/layout/Navigation"
-import { useHistory } from "react-router"
+import Navigation from "../components/layout/Navigation";
+import { useHistory } from "react-router";
 //Contact page requirements - > Email form (name,email, category selection, message)
 
 //Also place in a section with contact links (phone number, email address, social icons and links)
 export default function Contact() {
-
-    return(
-
-        <div>
-            <Navigation />
-            <div className="ui column stackable very padded relaxed container">
-                <form action="https://formsubmit.co/outofcontrolmike@gmail.com" method="POST" className="ui form">
-                <input type="hidden" name="_subject" value="Email from portfolio"/>
-                    <input type="hidden" name="_next" value="http://localhost:3000/thanks"/>
-                    <input type="hidden" name="_captcha" value="false"/>
-                    <div className="field">
-                        <label>Full Name</label>
-                        <input id="name" type="text" required name="name" placeholder="Full Name" />
-                    </div>
-                    <div className="field">
-                        <label>Email Address</label>
-                        <input id="email" type="text" required name="email" placeholder="Prefered Email Address" />
-                    </div>
-                    <div className="field">
-                        <label>Category</label>
-                        <select type="text" name="category" id="category" className="ui fluid dropdown">
-                            <option value="">Category</option>
-                            <option>General</option>
-                            <option>Suggestion</option>
-                            <option>Complaint</option>
-                            <option>Compliment</option>
-                        </select>
-                    </div>
-                    <div className="field">
-                        <label>Message</label>
-                        <textarea id="message" required type="text" name="message" placeholder="Message" />
-                    </div>
-                    <button className="ui button orange" id="submit" type="submit">Submit</button>
-                </form>
-            </div>
-
-        </div>
-    )
+  return (
+    <div>
+      <Navigation />
+      <div className="ui column stackable very padded relaxed container">
+        <form
+          action="https://formsubmit.co/outofcontrolmike@gmail.com"
+          method="POST"
+          className="ui form large"
+        >
+          <input type="hidden" name="_subject" value="Email from portfolio" />
+          <input
+            type="hidden"
+            name="_next"
+            value="http://localhost:3000/thanks"
+          />
+          <input type="hidden" name="_captcha" value="false" />
+          <div className="field">
+            <label>Full Name</label>
+            <input
+              id="name"
+              type="text"
+              required
+              name="name"
+              placeholder="Full Name"
+            />
+          </div>
+          <div className="field">
+            <label>Email Address</label>
+            <input
+              id="email"
+              type="text"
+              required
+              name="email"
+              placeholder="Prefered Email Address"
+            />
+          </div>
+          <div className="field">
+            <label>Category</label>
+            <select
+              type="text"
+              name="category"
+              id="category"
+              className="ui fluid dropdown"
+              style={{ color: "gold" }}
+            >
+              <option style={{ color: "grey" }} default value="">
+                Category
+              </option>
+              <option style={{ color: "white" }}>General</option>
+              <option style={{ color: "white" }}>Suggestion</option>
+              <option style={{ color: "white" }}>Complaint</option>
+              <option style={{ color: "white" }}>Compliment</option>
+            </select>
+          </div>
+          <div className="field">
+            <label>Message</label>
+            <textarea
+              id="message"
+              required
+              type="text"
+              name="message"
+              placeholder="Message"
+            />
+          </div>
+          <button
+            className="ui button yellow fluid basic"
+            id="submit"
+            type="submit"
+            style={{ fontSize: "1.5rem" }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
