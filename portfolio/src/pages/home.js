8b62d352b635bcabcb53a876140c1e2422f89c31
Michael Wilson <outofcontrolmike@gmail.com>
2021-12-22 20:55:35 -0600

    style:  landingPage

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -1,41 +1,21 @@
 import picture from "../assets/img/profilePic-removebg-preview.png";
-import myPicture from "../assets/img/profilePic-removebg-preview.png";
-
-import { Link } from "react-router-dom";
-
 function Home() {
-  // function handleHover() {
-  //     document.getElementById('landingPage').style.filter = "brightness(80%)";
-  //     document.getElementById('blink').style.animation = "blinker 0s linear infinite"
-  // }
-  // function handleLeave() {
-  //     document.getElementById('landingPage').style.filter = "brightness(50%)";
-  //     document.getElementById('blink').style.animation = "blinker 10s linear infinite"
 
-  // }
 
   return (
-    // <div id="overlay">
-    //     <img className="ui image fluid" id="landingPage" src={picture}/>
-    //     <a id="landingPageText" className="ui">
-    //         <Link id="link"to="/about">
-    // <h1><span className="ui text large" id="blink" onMouseOver={handleHover} onMouseLeave={handleLeave} >Let's Achieve Goals Together!</span></h1>
-    //     </Link></a>
-    // </div>
-
     <div className="ui two column stackable grid" id="landingPage">
       <div className="ui column" style={{margin:"auto", textAlign: "center"}}>
         <span style={{ textAlign: "left", color: "gold" }} className="ui text gold massive">
           Michael A. Wilson
         </span>
     
-    <h2>"The cave you're afraid to enter, holds the treasures you seek..."</h2>    
+    <h2 id="landingPageSub">Aspring Front End Developer Currently Working With React</h2>    
         <div>
 
-          <button className="ui button massive basic yellow" style={{marginTop: "2rem"}}>
+          <button className="ui button massive inverted basic learnMore yellow" style={{marginTop: "2rem"}}>
               <a
                 href="about"
-                style={{color: "yellow"}}
+                style={{color: "gold", outlineColor : "blue"}}
               >
                 Learn More </a>
             
@@ -43,7 +23,7 @@ function Home() {
         </div>
       </div>
       <div className="ui column">
-        <img className="ui image big center aligned " style={{textAlign: "center", marginLeft: "15%"}}  src={picture}></img>
+        <img className="ui image big center aligned" style={{textAlign: "center", marginLeft: "15%"}}  src={picture}></img>
       </div>
     </div>
   );
