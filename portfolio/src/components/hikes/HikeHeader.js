c066083c1fe40646734cddead660c2a2e9b9a1aa
Michael Wilson <mike@beardeddevops.com>
2022-01-04 12:46:37 -0600

    wip: working hikes project

--- portfolio/src/components/hikes/HikeHeader.js
+++ portfolio/src/components/hikes/HikeHeader.js
@@ -4,8 +4,8 @@ function HikeHeader() {
   return (
     <div className="ui">
       <h1
-        className="ui text header massive center aligned"
-        style={{ fontSize: "6rem", padding: "1rem" }}
+        className="ui text header center aligned"
+        style={{ fontSize: "2rem", padding: ".3rem" }}
       >
         417 Missouri Hikes
       </h1>
