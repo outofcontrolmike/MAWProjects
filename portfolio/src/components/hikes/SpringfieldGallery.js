0c0d2ba460f958dc59169e1edf68832427a5dbab
Michael Wilson <outofcontrolmike@gmail.com>
2022-01-10 21:02:48 -0600

    wip: styling spfGallery

--- portfolio/src/components/hikes/SpringfieldGallery.js
+++ portfolio/src/components/hikes/SpringfieldGallery.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function SpringfieldGallery() {
 
-    const spfImages = ["img/spfPhotos/1.jpg","img/spfPhotos/2.jpg","img/spfPhotos/3.jpg","img/spfPhotos/4.jpg","img/spfPhotos/5.jpg","img/spfPhotos/6.jpg","img/spfPhotos/7.jpg","img/spfPhotos/8.jpg","img/spfPhotos/9.jpg","img/spfPhotos/10.jpg","img/spfPhotos/11.jpg","img/spfPhotos/12.jpg","img/spfPhotos/13.jpg","img/spfPhotos/14.jpg","img/spfPhotos/15.jpg","img/spfPhotos/16.jpg","img/spfPhotos/17.jpg","img/spfPhotos/18.jpg","img/spfPhotos/20.jpg"];
+    const spfImages = ["img/spfPhotos/1.jpg","img/spfPhotos/2.jpg","img/spfPhotos/3.jpg","img/spfPhotos/4.jpg","img/spfPhotos/5.jpg","img/spfPhotos/6.jpg","img/spfPhotos/7.jpg","img/spfPhotos/8.jpg","img/spfPhotos/9.jpg","img/spfPhotos/10.jpg","img/spfPhotos/11.jpg","img/spfPhotos/12.jpg","img/spfPhotos/13.jpg","img/spfPhotos/14.jpg","img/spfPhotos/15.jpg","img/spfPhotos/16.jpg","img/spfPhotos/17.jpg","img/spfPhotos/18.jpg","img/spfPhotos/19.jpg","img/spfPhotos/20.jpg"];
 
     function createImages() {
-        for(let i = 1; i <= spfImages.length; i++ )
+        for(let i = 0; i < spfImages.length; i++ )
         {
            let spfImage =  document.createElement('img');
            spfImage.src = spfImages[i];
-           spfImage.classList="ui huge image centered padded ";
+           spfImage.classList="ui huge image centered";
+           spfImage.style.padding = "1rem"
            console.log("spfImage", spfImage.src);
            document.getElementById('spfList').appendChild(spfImage);
         }
@@ -19,8 +20,8 @@ export default function SpringfieldGallery() {
     }, 10);
     
     return (
-        <div>
-        <div id="spfList" className='ui images'>
+        <div className="ui" style={{backgroundColor: "white"}}>
+        <div id="spfList" className='ui images centered'>
             
         </div>
         </div>
