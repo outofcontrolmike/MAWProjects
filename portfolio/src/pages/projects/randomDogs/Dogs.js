57e8258edabce4ffb1203078079b8e29a3c591f4
Michael Wilson <outofcontrolmike@gmail.com>
2022-01-27 21:50:04 -0600

    wip: randomDog project

--- portfolio/src/pages/projects/randomDogs/Dogs.js
+++ portfolio/src/pages/projects/randomDogs/Dogs.js
@@ -1,5 +1,5 @@
 import React from 'react';
 // - https://dog.ceo/dog-api/
 export default function Dogs() {
-  return <div>Test for dogs</div>;
+  return <div style={{backgroundColor: "white", height: "100vh", color: "black"}}><span className='ui text massive black centered align"'>Random Dog Viewer</span></div>;
 }
