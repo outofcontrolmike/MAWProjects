1f1773cbc7d8c43ab81f287c99ca992a2ca86436
Michael Wilson <mike@beardeddevops.com>
2022-01-28 14:04:19 -0600

    wip: styling randomDog project

---
+++ portfolio/src/components/dogs/DogFooter.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//Dog Footer
+export default function DogFooter() {
+  return (
+    <div className="ui container footer fluid" id="dogFooter">
+      Supposed to be a credits link and a back to projects here
+    </div>
+  );
+}
--- portfolio/src/pages/projects/randomDogs/Dogs.js
+++ portfolio/src/pages/projects/randomDogs/Dogs.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React from "react";
+import DogFooter from "../../../components/dogs/DogFooter";
 // - https://dog.ceo/dog-api/
 export default function Dogs() {
-
   function handleClick() {
     let url = "https://dog.ceo/api/breeds/image/random";
-    fetch(url).then((response) => response.json())
-    .then((dog) => {
-      console.log("dog", dog);
-      document.getElementById('dogImage').src = dog.message;
-      
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-  };
+    fetch(url)
+      .then((response) => response.json())
+      .then((dog) => {
+        console.log("dog", dog);
+        console.log("this", this);
+        console.log("dogs stuff...", dog.message);
+        console.log("dogMessage", dog.message.clientHeight);
+        let dogImage = document.getElementById("dogImage");
+        dogImage.src = dog.message;
+        var width = dogImage.clientWidth;
+        var height = dogImage.clientHeight;
 
-  return <div style={{backgroundColor: "white", height: "100vh", color: "black"}}>
-       <span className='ui text massive black centered align"'>Random Dog Viewer</span>
-       <hr></hr>
-      <div className='dogContainer'>
-          <img id="dogImage" src="" alt="dog image">
+        console.log(width, height);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-          </img>
+  return (
+    <div
+      className="ui container fluid segment stackable "
+      id="dogsProject"
+      style={{ backgroundColor: "white", height: "1200px", color: "black" }}
+    >
+      <p
+        className="ui text massive black"
+        style={{ textAlign: "center", fontSize: "4.5rem" }}
+        id="dogHeader"
+      >
+        Random Dog Viewer
+      </p>
+
+      <div id="dogContainer" className="ui container">
+        <img
+          className="ui image centered rounded"
+          id="dogImage"
+          src="img/redDog.PNG"
+          alt="dog image"
+        ></img>
+
+        <div className="ui container" id="dogBtnContainer">
+          <button
+            className="ui button huge black fluid circular"
+            id="dogBtn"
+            onClick={handleClick}
+          >
+            <i className="paw icon huge" style={{ paddingLeft: "1.5rem" }}></i>
+          </button>
+        </div>
       </div>
-        <button className='ui button huge blue' onClick={handleClick}>Grab a random Dog</button>
-  </div>;
+      <DogFooter />
+    </div>
+  );
 }
