b83d9738234a705fef6320a8aa834e342816aae8
Michael Wilson <mike@beardeddevops.com>
2022-02-01 09:11:47 -0600

    wip: catsList compnent (data is appending to list)  Just need to work on design

--- portfolio/src/components/cats/CatsList.js
+++ portfolio/src/components/cats/CatsList.js
@@ -1,37 +1,48 @@
-import React from 'react';
-import CatNavigation from './CatNavigation';
+import React from "react";
+import CatNavigation from "./CatNavigation";
 
 //Cat List
 export default function CatsList(props) {
-
   let pageNum = 1;
 
-    //LoadFacts
-    function loadFacts() {
-
-      //Figure out how to get every random fact by button click (like append or prepend to list)
-      if(pageNum <= 34) {
-        console.log('facts List', pageNum)
+  //LoadFacts
+  function loadFacts() {
+    //Figure out how to get every random fact by button click (like append or prepend to list)
+    if (pageNum <= 34) {
+      console.log("facts List", pageNum);
       let url = "https://catfact.ninja/facts?page=" + pageNum;
       pageNum++;
       fetch(url)
         .then((response) => response.json())
         .then((catFacts) => {
-            console.log("catFact", catFacts);
+          catFacts.data.map(createList);
+          let hr = document.createElement("hr");
+          document.getElementById("catFactsList").append(hr);
         })
         .catch((error) => {
           console.error("Error:", error);
         });
-      
-      }
-      else {
-        pageNum = 1;
-      }
+    } else {
+      pageNum = 1;
     }
-  return <div>
-    <CatNavigation />
-      <h3>CatsList Test</h3>
-      <button onClick={loadFacts} className='ui button huge red basic'>Load Lists</button>
+  }
 
-  </div>;
+  function createList(fact) {
+    console.log(fact);
+    let item = document.createElement("p");
+    item.classList("ui text large");
+    item.innerHTML = fact.fact;
+    document.getElementById("catFactsList").append(item);
+  }
+
+  return (
+    <div className="ui container fluid center aligned">
+      <CatNavigation />
+      <h3>CatsList Test</h3>
+      <button onClick={loadFacts} className="ui button huge red basic">
+        Load Lists
+      </button>
+      <div className="ui container segment piled" id="catFactsList"></div>
+    </div>
+  );
 }
