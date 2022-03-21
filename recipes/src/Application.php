2f61d762a028896658bcf405a420f5dc5ffe0eac
Michael Wilson <outofcontrolmike@gmail.com>
2022-03-21 17:32:02 -0500

    wip: recipes project - Authentication complete

--- recipes/src/Application.php
+++ recipes/src/Application.php
@@ -28,6 +28,17 @@ use Cake\ORM\Locator\TableLocator;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 
+
+//Authentication 
+
+use Authentication\AuthenticationService;
+use Authentication\AuthenticationServiceInterface;
+use Authentication\AuthenticationServiceProviderInterface;
+use Authentication\Middleware\AuthenticationMiddleware;
+use Cake\Routing\Router;
+use Psr\Http\Message\ServerRequestInterface;
+
+
 /**
  * Application setup class.
  *
@@ -35,6 +46,7 @@ use Cake\Routing\Middleware\RoutingMiddleware;
  * want to use in your application.
  */
 class Application extends BaseApplication
+    implements AuthenticationServiceProviderInterface
 {
     /**
      * Load all the application configuration and bootstrap logic.
@@ -91,6 +103,7 @@ class Application extends BaseApplication
             // using it's second constructor argument:
             // `new RoutingMiddleware($this, '_cake_routes_')`
             ->add(new RoutingMiddleware($this))
+            ->add(new AuthenticationMiddleware($this))
 
             // Parse various types of encoded request bodies so that they are
             // available as array through $request->getData()
@@ -105,6 +118,35 @@ class Application extends BaseApplication
 
         return $middlewareQueue;
     }
+
+    public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface
+{
+    $authenticationService = new AuthenticationService([
+        'unauthenticatedRedirect' => Router::url('/users/login'),
+        'queryParam' => 'redirect',
+    ]);
+
+    // Load identifiers, ensure we check email and password fields
+    $authenticationService->loadIdentifier('Authentication.Password', [
+        'fields' => [
+            'username' => 'email',
+            'password' => 'password',
+        ]
+    ]);
+
+    // Load the authenticators, you want session first
+    $authenticationService->loadAuthenticator('Authentication.Session');
+    // Configure form data check to pick email and password
+    $authenticationService->loadAuthenticator('Authentication.Form', [
+        'fields' => [
+            'username' => 'email',
+            'password' => 'password',
+        ],
+        'loginUrl' => Router::url('/users/login'),
+    ]);
+
+    return $authenticationService;
+}
 
     /**
      * Register application container services.
---
+++ recipes/src/Model/Entity/User.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Authentication\PasswordHasher\DefaultPasswordHasher;
+use Cake\ORM\Entity;
+
+/**
+ * User Entity
+ *
+ * @property int $id
+ * @property string $user_name
+ * @property string $email
+ * @property string $password
+ * @property string $bio
+ * @property string $instagram_link
+ * @property \Cake\I18n\FrozenTime|null $created
+ * @property \Cake\I18n\FrozenTime|null $modified
+ *
+ * @property \App\Model\Entity\Recipe[] $recipes
+ */
+class User extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'user_name' => true,
+        'email' => true,
+        'password' => true,
+        'bio' => true,
+        'instagram_link' => true,
+        'created' => true,
+        'modified' => true,
+        'recipes' => true,
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array
+     */
+    protected $_hidden = [
+        'password',
+    ];
+
+     // Add this method
+     protected function _setPassword(string $password) : ?string
+     {
+         if (strlen($password) > 0) {
+             return (new DefaultPasswordHasher())->hash($password);
+         }
+     }
+}
