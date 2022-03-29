072aa0dca5000befc36d760616ed383205f301d4
Michael Wilson <mike@beardeddevops.com>
2022-03-29 07:43:26 -0500

    style: login page

--- recipes/templates/Users/login.php
+++ recipes/templates/Users/login.php
@@ -1,4 +1,4 @@
-<div class="users form">
+<div class="users form container content">
     <?= $this->Flash->render() ?>
     <h3>Login</h3>
     <?= $this->Form->create() ?>
@@ -7,7 +7,7 @@
         <?= $this->Form->control('email', ['required' => true]) ?>
         <?= $this->Form->control('password', ['required' => true]) ?>
     </fieldset>
-    <?= $this->Form->submit(__('Login')); ?>
+    <?= $this->Form->submit(__('Login'), ['action' => ['add']]); ?>
     <?= $this->Form->end() ?>
 
     <?= $this->Html->link("If you don't have an account yet, Please sign up!", ['action' => 'add']) ?>
