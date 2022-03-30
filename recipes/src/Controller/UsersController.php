82c9c9849e415e87b3f7efc9b5200125747a7d3c
Michael Wilson <mike@beardeddevops.com>
2022-03-30 08:09:43 -0500

    change: redirect for users

--- recipes/src/Controller/UsersController.php
+++ recipes/src/Controller/UsersController.php
@@ -57,7 +57,7 @@ class UsersController extends AppController
             if ($this->Users->save($user)) {
                 $this->Flash->success(__('The user has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['controller'=> "Recipes",'action' => 'index']);
             }
             $this->Flash->error(__('The user could not be saved. Please, try again.'));
         }
