8cdac8c8b2a96fed095e43b2631a5d1e0fffeeb4
Michael Wilson <outofcontrolmike@gmail.com>
2022-04-24 17:08:00 -0500

    wip: landing page

--- recipes/src/Controller/AppController.php
+++ recipes/src/Controller/AppController.php
@@ -61,6 +61,6 @@ class AppController extends Controller
     // Public recipes viewable by non-logged in users
     // for all controllers in our application, make index and view, public
     // actions public, skipping the authentication check
-    $this->Authentication->addUnauthenticatedActions(['index','public', 'view']);
+    $this->Authentication->addUnauthenticatedActions(['index','public', 'view', 'contact','tags']);
 }
 }
--- recipes/templates/Pages/home.php
+++ recipes/templates/Pages/home.php
@@ -22,6 +22,7 @@ use Cake\Datasource\ConnectionManager;
 use Cake\Error\Debugger;
 use Cake\Http\Exception\NotFoundException;
 
+
 $this->disableAutoLayout();
 
 ?>
@@ -45,18 +46,63 @@ $this->disableAutoLayout();
     <?= $this->fetch('script') ?>
 </head>
 
-<body>
-    <header>
-        <div class="container text-center">
-            <a href="http://localhost:8765/recipes" target="_self" rel="noopener">
+<body class="container">
+    <nav class="top-nav">
+        <div class="top-nav-title">
+            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
+        </div>
+        <div class="top-nav-links">
+            <a target="_self" rel="noopener" href="http://localhost:8765/users/login">Login |</a>
+            <a target="_self" rel="noopener" href="http://localhost:8765/users/add">Create Account |</a>
+            <a target="_self" rel="noopener" href="http://localhost:8765/users/contact">Contact |</a>
+            <a target="_self" rel="noopener" href="http://localhost:8765/users/faq">FAQ</a>
+
+            <!-- <a target="_self" rel="noopener" href="http://localhost:8765/recipes">Public Recipes |</a> -->
+        </div>
+
+    </nav>
+
+    <div class="container text-center content" style="margin-bottom: 2rem;">
+        <p>Do you need one place for sharing and storing your recipes? If so, look no further. This website allows viewers like you to view other creator's recipes as well as list/manage your own. If you don't want to sign up for an account, that's perfectly fine. You will always have the option to view all the recipes on this site without an account.</p>
+        <hr>
+        <div>
+            </>
+            <form name="form" action="" method="post">
+                <input type="text" name="subject" id="subject" value="Car Loan"></>
+                
+                <?= $this->Html->link(__('Submit Search'), ['controller' => "Recipes/", 'action' => 'tagged', $_GET['name']], ['class' => 'button']) ?>
+            </form>
+
+
+
+        </div>
+        <!-- <a href="http://localhost:8765/recipes" target="_self" rel="noopener">
                 <img alt="CakePHP" src="https://cakephp.org/v2/img/logos/CakePHP_Logo.svg" width="350" />
             </a>
             <h1>
                 Welcome to Recipes Project
-            </h1>
-
+            </h1> -->
+    </div>
+    <div class="container text-center content" style="margin-bottom: 2rem;">
+        <img alt="Recipe Card Demo" src="https://lh3.googleusercontent.com/Oh-fE5PgCxTV4qn5UCXJnUK4ZHWXCzC16AWp8WpN-FMMzLVfV3A6eb2k9QePcm7e3Y_BLfnwHX9Cvu8T1rvnpaTDn3Wwh4bC_5j-xHUkMz1jvEjrElN0nzczokS9w8nWqichsFDrrRvTQpYLlKvnXaat3-mDJHoQRUvGkKdlb3obdYouBX--tm73iV-6QEPk55PT7eRAEcB9RsPpJ7lOdaGTfcEOcVbXSMiofJ7nRPDNIwrynBc_6-g46FZjcTo_ZR3MzAJJ5efw8EwwN0bs-_qSv52Ujg6fFmnvj35DmP-Ir_lDlFvBXVwchDAKwTlY6Egaaz5wCAP4b14jAT_pEZdilfq1Mdq9kTTRBwV1gp2VIY3zvXU9UygnLrrOkXEqr-joiUyRdE6euFpcY2IT1pmIMzMd9UDbOWDpZywMiCtb1fwcUUhap1GOrc59Hp7--BG4UJh0uoieoEbD8Mhu309s8P__B94GiuRbhwTpHU2llV541-9JcmadlHete1mjFoMA469AVniBKbEc8xWjapsT2KxsWZVhVOI-x6lGq2BY_OvW--2Tqn9rcVJn_zG_wBfyTuIecg0hAbSqoysYPLWdv2TkwvXFwAvF4roS-Z80ebV9CkP8JOaw0aQO33bz1CwroDSXGvSTSOafEkX7jQjhmbVhklW9_7DKk5eiZ24UDNgxgCs95j46Pykg6RIqj65Obs53tMN3qD-QTuKAObIUXnOtQ95Exs-rmRFP_SO_xZp_Jl6oJz7f3-wBJj3rRREWrTOE1MDYXIVDk0NCccm9JtIuS0EBrjdyKL4iJIprL3lZ5wa8KlQJRpIIB59U5N-mR-bzaaS6lDOmBugS7u6p9OH56EWrNpxsTakk=w1116-h703-no?authuser=1" width="90%" />
+        <hr>
+        <div>
+            <?= $this->Html->link(__('View Recipes'), ['controller' => "Recipes", 'action' => 'index'], ['class' => 'button']) ?>
         </div>
-    </header>
+    </div>
+    <div class="container text-center content">
+        <img alt="Recipe Card Demo" src="https://lh3.googleusercontent.com/-FtZw2mXRkb3sUbjgexVXE9rswHjcLVnRkb31ktDo60m3Sw4QhrkhHJDNTAbTr7Z1Iuv00D0Lt15mAyKBTw7bVQeuBPbGunsbPoteR7sDbpWM8pAEZohJEhXiAErpK5BjomWW6YqMwcKjCMvVFAUudQy1tzC3e9F0ggo554qtuQ7YbK5xTJ0WyWlEXDC_L4AN1meoH7HhxZAHvFGrD86iULx0CMxZtrPtVVNlZKcAllxELadTcaSUx4d11Q2XzH9o5hOYFWSckt5pkSW4h7yjRnJ7nKL13G2HL7I6mnLF84pLwvox27aiP5IElCEVSvCaob0MWq_U3pLtvpVrRv7Uot0wW_VE2SzcNXVGsbMNFoNQhJAwNR3kgyhdr2oiiYfmxMVVEpO22EZC2DG1YUKsJzQswCgY0Ii5isydNe1JtEMi9aZnLao7t5j81mE_O390rx_LTxgoISo3EMBcbs0BFNRVaVKykMQUHXTMXzZw7r9XEiPJnVDGK47FZad7kZcY2kDmspnIKW7ZdNiMK2r7nqeBil4nyig9J-SrYhzOLAH72fgj6S59DGy8kwfdSI7lmhhPXPCl2rhGLRzTGgP4PU8gqXbNuIoTP2zsHW2OXSQoD3JzcI2TUrulRavqHGreOknHPT2u92dG8p-YJsM5cfwNereq56yYE9-d7TYMRh8OMmyAyG98whDzbpogp_6eR20MnABafAH9wlQVsNibuKIzv0ciOjOLuPLaTHqJPQDUuUToinflcFcwSVrahSMCQ-sHnH3UBHDUdqZ49U-vxPq8xqTrpstWfaV4Da1lI3l4d_LRaJoMJQ2r4AYxxiLexPRtvtwJ90bZHs3d8c91n-w5UMgXiI6maSlbqpW=w1112-h693-no?authuser=1" width="90%" />
+        <hr>
+        <div>
+            <?= $this->Html->link(__('Sign up for a profile!'), ['controller' => "Users", 'action' => 'add'], ['class' => 'button']) ?>
+        </div>
+    </div>
+    </div>
+    <div class="container content">
+        <footer>
+            <h6>Thank you for you using cake recipes cms. This was a dream come true with the help of cakePHP.</h6>
+        </footer>
+    </div>
 
 </body>
 
