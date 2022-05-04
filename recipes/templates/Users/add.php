823aa1db89b85d2fa061f72a8dfcee988c4f4f0b
Michael Wilson <mike@beardeddevops.com>
2022-05-04 09:03:49 -0500

    style: Sign up page - centering and form ui enhancement

--- recipes/templates/Users/add.php
+++ recipes/templates/Users/add.php
@@ -6,23 +6,25 @@
  */
 ?>
 
-    <div class="ui container segment raised very padded">
+<div class="ui container fluid" id="signUpFormContainer">
+    <div class="ui container segment raised very padded" id="signUpForm">
         <div class="ui form" id="">
             <?= $this->Form->create($user) ?>
-                <legend style="text-align:center">
-                    <h2><?= __('Sign up to start adding recipes!') ?></h2>
-                </legend>
-                <?php
-                echo $this->Form->control('user_name',['class' => "field"]);
-                echo $this->Form->control('title');
-                echo $this->Form->control('email');
-                echo $this->Form->control('password');
-                echo $this->Form->control('bio');
-                echo $this->Form->control('image');
-                echo $this->Form->control('link');
-                echo $this->Form->control('instagram_link');
-                ?>
-            <?= $this->Form->button(__('Submit'), ['class' => 'ui button red massive']) ?>
+            <legend style="text-align:center">
+                <h2><?= __('Sign up to start adding recipes!') ?></h2>
+            </legend>
+            <?php
+            echo $this->Form->control('user_name', ['class' => "field"]);
+            echo $this->Form->control('title');
+            echo $this->Form->control('email');
+            echo $this->Form->control('password');
+            echo $this->Form->control('bio');
+            echo $this->Form->control('image');
+            echo $this->Form->control('link');
+            echo $this->Form->control('instagram_link');
+            echo '<br>';
+            ?>
+            <?= $this->Form->button(__('Submit'), ['class' => 'ui button red fluid']) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
