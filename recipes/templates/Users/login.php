1ac0adfac1514a26a2902bc8d89265002a237f4d
Michael Wilson <mike@beardeddevops.com>
2022-05-04 08:59:32 -0500

    style: login page a bit better

--- recipes/templates/Users/login.php
+++ recipes/templates/Users/login.php
@@ -1,17 +1,20 @@
-<div class="ui container segment very padded relaxed raised " id="loginForm">
-    <?= $this->Flash->render() ?>
-    <h3>Login</h3>
-    <?= $this->Form->create() ?>
-    <div class="ui form">
-        <fieldset>
-            <legend><?= __('Please enter your email and password') ?></legend>
-            <?= $this->Form->control('email', ['required' => true]) ?>
-            <?= $this->Form->control('password', ['required' => true]) ?>
+<div class="ui container fluid" id="loginFormContainer">
+    <div class="ui container segment very padded relaxed raised " id="loginForm">
+        <?= $this->Flash->render() ?>
+        <h3>Login</h3>
+        <?= $this->Form->create() ?>
+        <div class="ui form">
+            <fieldset>
+                <legend><?= __('Please enter your email and password') ?></legend>
+                <?= $this->Form->control('email', ['required' => true]) ?>
+                <?= $this->Form->control('password', ['required' => true]) ?>
 
-        </fieldset>
-        <?php echo $this->Form->button('Submit Form', ['type' => 'submit', 'class' => 'ui button red']) ?>
+            </fieldset>
+            <br>
+            <?php echo $this->Form->button('Submit Form', ['type' => 'submit', 'class' => 'ui button red fluid']) ?>
+        </div>
+        <?= $this->Form->end() ?>
+        <h5>OR</h5>
+        <?= $this->Html->link('Sign Up', ['action' => 'add'], ['class' => 'ui button teal fluid'], ['style' => 'width:']) ?>
     </div>
-    <?= $this->Form->end() ?>
-    <h5>OR</h5>
-    <?= $this->Html->link('Sign Up', ['action' => 'add'], ['class' => 'ui button'], ['style' => 'width:']) ?>
 </div>
