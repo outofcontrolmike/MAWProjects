901b078faeecc15ccc40ca0b3d121e43d5133fdd
Michael Wilson <mike@beardeddevops.com>
2022-05-04 12:29:05 -0500

    fix: date formating recipes view

--- recipes/templates/Recipes/view.php
+++ recipes/templates/Recipes/view.php
@@ -7,7 +7,9 @@ $creator = $_SESSION["Auth"]["id"];
 <div class="ui very padded relaxed raised segment container center aligned ">
     <h1><?= h($recipe->title) ?></h1>
     <?php if ($recipe->image != null) : ?>
-        <p><?= $this->Html->image($recipe->image) ?></p>
+        <?= $this->Html->image($recipe->image, ['class' => "ui image centered medium"]) ?>
+    <?php else : ?>
+        <?= $this->Html->image('comingSoon.jpg', array('alt' => 'CakePHP', 'border' => '0', 'data-src' => 'holder.js/100%x25', 'height' => "200px")); ?></a>
     <?php endif ?>
     <p><small>PrepTime: <?= $recipe->prep_time ?> min</small></p>
     <p><small>CookTime: <?= $recipe->cook_time ?> min</small></p>
@@ -20,8 +22,8 @@ $creator = $_SESSION["Auth"]["id"];
     <h4>Directions: <?= h($recipe->directions) ?></h4>
     </p>
     <p><b>Tags:</b> <?= h($recipe->tag_string) ?></p>
-    <p><small>Created: <?= $recipe->created->format(DATE_RFC850) ?></small></p>
-    <p><small>Last Modified: <?= $recipe->created->format(DATE_RFC850) ?></small></p>
+    <p><small>Created: <?= date_format($recipe->created, "m/d/Y") ?></small></p>
+    <p><small>Last Modified: <?= date_format($recipe->modified, "m/d/Y") ?></small></p>
     <p>Created By: <?= $recipe->user_id ?></p>
     <?php if ($creator === $recipe->user_id) : ?>
         <hr>
