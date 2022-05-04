1726a5e7dd80b8b6c5b9466f67a6729ed9551d60
Michael Wilson <mike@beardeddevops.com>
2022-05-04 11:45:51 -0500

    style: recipes index - convert to cards

--- recipes/templates/Recipes/index.php
+++ recipes/templates/Recipes/index.php
@@ -5,21 +5,36 @@
         <input name="test" id="recipeKeyword" value="" placeholder="...Type a keyword into here">
         <button onclick="submitKeyword()" class="button">Submit Keyword</button>
     </div>
-    <div class="index" id="recipeCardsIndex">
+    <div class="ui cards centered container fluid" id="recipeCardsIndex">
         <!-- <?= $this->Html->link('Add Recipe', ['action' => 'add'], ['class' => 'button']) ?> -->
 
         <!-- Here is where we iterate through our $Recipes query object, printing out recipe info -->
-        <?php foreach ($recipes as $recipe) : ?>
-            <div id="recipeCard" class="content container">
-                <?php if ($recipe->image != null) : ?>
-                    <?= $this->Html->image($recipe->image, ['class' => "ui small image"]) ?>
-                <?php endif ?>
-                <!-- <img src="<?= $recipe->image ?>" height="250px" width:="425px" /> -->
-                <h5> <?= $this->Html->link($recipe->title, ['action' => 'view', $recipe->slug]) ?></h5>
-                <p> <?= $recipe->body ?> </p>
-                <hr>
-                <em> Created By: <a href="http://localhost:8765/users/view/<?= $recipe->user_id ?>"><?= $recipe->user_id ?></a> </em>
-                <p>Listed: <?= $recipe->created->format(DATE_RFC850) ?></p>
+        <?php
+
+        foreach ($recipes as $recipe) : ?>
+            <div class="ui card ">
+                <div class="image" id="recipeImage">
+                    <?php if ($recipe->image != null) : ?>
+                        <?= $this->Html->image($recipe->image, ['class' => "ui image"]) ?>
+                    <?php else : ?>
+                        <?= $this->Html->image('comingSoon.jpg', array('alt' => 'CakePHP', 'border' => '0', 'data-src' => 'holder.js/100%x25')); ?></a>
+                    <?php endif ?>
+                </div>
+                <div class=" content">
+                    <div class="header" style="text-decoration: none"><?= $this->Html->link($recipe->title, ['action' => 'view', $recipe->slug]) ?></div>
+                    <div class="description">
+                        <?= $recipe->body ?>
+                    </div>
+                </div>
+                <div class="extra content">
+                    <span class="right floated">
+                        <?= date_format($recipe->created, "m/d/Y") ?>
+                    </span>
+                    <span>
+                        <i class="user icon"></i>
+                        Created By: <a href="http://localhost:8765/users/view/<?= $recipe->user_id ?>"><?= $recipe->user_id ?></a>
+                    </span>
+                </div>
             </div>
         <?php endforeach; ?>
     </div>
