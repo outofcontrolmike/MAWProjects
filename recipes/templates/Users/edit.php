7dd68634fb00e7d95c0424402c93f8b332fbc47d
Michael Wilson <mike@beardeddevops.com>
2022-05-09 11:46:29 -0500

    style: Users Edit

--- recipes/templates/Users/edit.php
+++ recipes/templates/Users/edit.php
@@ -23,19 +23,66 @@
     <div class="ui very padded relaxed raised column container segment">
         <div class="ui form">
             <?= $this->Form->create($user) ?>
-            <legend><?= __('Edit Your Account Details') ?></legend>
-            <?php
-            echo $this->Form->control('user_name');
-            echo $this->Form->control('title');
-            echo $this->Form->control('email');
-            echo $this->Form->control('password');
-            echo $this->Form->control('bio');
-            echo $this->Form->control('image');
-            echo $this->Form->control('link', ['placeholder' => "Enter your website name without the https or http, example: mawportfolio.online"]);
-            echo $this->Form->control('instagram_link',['placeholder' => "Enter your Instagram user name and we'll link to it.  example: explodingsnes"]);
-            ?>
-            <br>
-            <?= $this->Form->button(__('Submit'), ['class' => 'ui fluid button yellow']) ?>
+            <h1 style="text-align: center;">
+                <legend><?= __('Edit Your Account Details') ?></legend>
+            </h1>
+            <div class=" ui field">
+                <label>User Name</label>
+                <?php
+                echo $this->Form->control('user_name', ['label' => ""]);
+                ?>
+            </div>
+
+            <div class="ui field">
+                <label>Your Title</label>
+                <?php
+                echo $this->Form->control('title', ['label' => ""]);
+                ?>
+            </div>
+
+            <div class="ui field">
+                <label>Email</label>
+                <?php
+                echo $this->Form->control('email', ['label' => ""]);
+                ?>
+            </div>
+
+            <div class="ui field">
+                <label>Password</label>
+                <?php
+                echo $this->Form->control('password', ['label' => ""]);
+                ?>
+            </div>
+
+            <div class="ui field">
+                <label>Biography</label>
+                <?php
+                echo $this->Form->control('bio',  ['label' => "", 'placeholder' => "Enter a new line for each step", 'rows' => '3', 'class' => "ui input"]);
+                ?>
+            </div>
+
+            <div class="ui field">
+                <label>Profile Picture</label>
+                <?php
+                echo $this->Form->control('image', ['label' => ""]);
+                ?>
+            </div>
+            <div class="ui field">
+                <label>Business Link</label>
+                <?php
+                echo $this->Form->control('link', ['label' => "", 'placeholder' => "Enter your website or portfioli and we'll link to it.  example: mawportfolio.online"]);
+                ?>
+            </div>
+
+            <div class="ui field">
+                <label>Instagram Link</label>
+                <?php
+                echo $this->Form->control('instagram_link', ['label' => "", 'placeholder' => "Enter your Instagram user name and we'll link to it.  example: explodingsnes"]);
+
+                ?>
+            </div>
+            <!-- End -->
+            <?= $this->Form->button(__('Submit'), ['class' => 'ui fluid button red']) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
