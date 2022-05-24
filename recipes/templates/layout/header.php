04692ea703236c53136bc5464ba24b8b78805780
Michael Wilson <mike@beardeddevops.com>
2022-05-24 14:40:31 -0500

    feat: plugged in google fonts

--- recipes/templates/layout/header.php
+++ recipes/templates/layout/header.php
@@ -18,6 +18,15 @@
 ?>
 <!DOCTYPE html>
 
+<head>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+
+</head>
+
 <body>
     <!-- Grab user ID to append to account settings -->
     <?php
