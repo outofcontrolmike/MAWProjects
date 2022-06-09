34adcb14ced47f068885c859b234210263a0bf7d
Michael Wilson <mike@beardeddevops.com>
2022-06-09 14:05:10 -0500

    wip: recipes view styling

--- recipes/templates/Recipes/view.php
+++ recipes/templates/Recipes/view.php
@@ -15,60 +15,125 @@ $recipeBody = preg_split('#(\r\n?|\n)+#', $recipe->body);
 ?>
 <br>
 
-<div class="ui two column container stackable grid segment tall stacked raised relaxed">
-    <div class="ui column container">
-        <!-- img -->
-        <div class="container">
-            <input type="hidden" value="" id="inputTest" />
-            <?php if ($recipe->image != null) : ?>
-                <?= $this->Html->image($recipe->image, ['class' => "ui image huge centered recipeCardImage"]) ?>
-            <?php else : ?>
-                <?= $this->Html->image('comingSoon.jpg', array('class' => "ui image huge recipeCardImage", 'alt' => 'CakePHP', 'border' => '0', 'data-src' => 'holder.js/100%x25', 'height' => "200px",)); ?></a>
-            <?php endif ?>
-        </div>
-        <i class="share icon teal large" onclick="shareURL();"></i>
 
-        <h1 class="ui text center aligned"><?= h($recipe->title) ?></h1>
-        <p>PrepTime: <?= $recipe->prep_time ?> min</p>
-        <p>CookTime: <?= $recipe->cook_time ?> min</p>
-        <p>Servings: <?= $recipe->servings ?> </p>
-        <p>Created: <?= date_format($recipe->created, "m/d/Y") ?></p>
-        <p>Created By: <?= $recipe->user_id ?></p>
-        <p><b>Tags:</b> <?= h($recipe->tag_string) ?></p>
-        <hr>
-        <div class="row" id="recipeBody">
-            <h1>Description</h1>
-            <?php foreach ($recipeBody as $bodyPart) {
-                $uppercaseFirst = ucfirst($bodyPart);
-                echo "<p>$uppercaseFirst</p>";
-            } ?>
-        </div>
+<!-- New recipe card -->
 
-    </div>
+<div class="ui container">
+    <div class="ui segment raised ">
+        <div class="ui two column doubling stackable grid container">
+            <div class="ui column">
+                <input type="hidden" value="" id="inputTest" />
+                <?php if ($recipe->image != null) : ?>
+                    <?= $this->Html->image($recipe->image, ['class' => "ui image  centered recipeCardImage"]) ?>
+                <?php else : ?>
+                    <?= $this->Html->image('comingSoon.jpg', array('class' => "ui big huge recipeCardImage", 'alt' => 'CakePHP', 'border' => '0', 'data-src' => 'holder.js/100%x25', 'height' => "200px",)); ?></a>
+                <?php endif ?>
+            </div>
+            <div class="ui column center aligned">
+                <i class="share icon teal large" onclick="shareURL();"></i>
+                <h1 class="ui text center aligned"><?= h($recipe->title) ?></h1>
+                <p>PrepTime: <?= $recipe->prep_time ?> min</p>
+                <p>CookTime: <?= $recipe->cook_time ?> min</p>
+                <p>Servings: <?= $recipe->servings ?> </p>
+                <p>Created: <?= date_format($recipe->created, "m/d/Y") ?></p>
+                <p>Created By: <?= $recipe->user_id ?></p>
+                <p><b>Tags:</b> <?= h($recipe->tag_string) ?></p>
 
-    <!-- 2nd column - Ingredients & Directions -->
-    <div class="ui column container" id="recipeView2ndColumn">
-        <div class="row">
-            <h1>Ingredients</h1>
-            <?php foreach ($ingredients as $ingredient) {
-                $uppercaseFirst = ucfirst($ingredient);
-                echo "<p>$uppercaseFirst</p>";
-            } ?>
+            </div>
         </div>
-        <br>
-        <hr>
-        <div class="row">
-            <h1>Directions</h1>
-            <?php $counter = 1; ?>
-            <?php foreach ($directions as $direction) {
-                $uppercaseFirst = ucfirst($direction);
-                echo "<b>Step $counter</b>";
-                echo "<p>$direction</p>";
-                $counter++;
-            } ?>
+        <div class="ui divider">
+
+        </div>
+        <div class="ui column very padded relaxed">
+            <p><span class="ui text large teal">
+                    <div class="row" id="recipeBody">
+                        <h1>Description</h1>
+                        <?php foreach ($recipeBody as $bodyPart) {
+                            $uppercaseFirst = ucfirst($bodyPart);
+                            echo "<p>$uppercaseFirst</p>";
+                        } ?>
+                    </div>
+                </span></p>
+
+            <p><span class="ui text large teal">
+                    <div class="row">
+                        <h1>Ingredients</h1>
+                        <?php foreach ($ingredients as $ingredient) {
+                            $uppercaseFirst = ucfirst($ingredient);
+                            echo "<p>$uppercaseFirst</p>";
+                        } ?>
+                    </div>
+                </span></p>
+            <div class="row">
+                <h1>Directions</h1>
+                <?php $counter = 1; ?>
+                <?php foreach ($directions as $direction) {
+                    $uppercaseFirst = ucfirst($direction);
+                    echo "<b>Step $counter</b>";
+                    echo "<p>$direction</p>";
+                    $counter++;
+                } ?>
+            </div>
         </div>
     </div>
 </div>
+
+<!-- End new card -->
+
+<!-- 
+<div class="ui container segment stacked raised relaxed">
+    <div class="ui column container">
+<div class="container">
+    <input type="hidden" value="" id="inputTest" />
+    <?php if ($recipe->image != null) : ?>
+        <?= $this->Html->image($recipe->image, ['class' => "ui image big centered recipeCardImage"]) ?>
+    <?php else : ?>
+        <?= $this->Html->image('comingSoon.jpg', array('class' => "ui big huge recipeCardImage", 'alt' => 'CakePHP', 'border' => '0', 'data-src' => 'holder.js/100%x25', 'height' => "200px",)); ?></a>
+    <?php endif ?>
+</div>
+<i class="share icon teal large" onclick="shareURL();"></i>
+
+<h1 class="ui text center aligned"><?= h($recipe->title) ?></h1>
+<p>PrepTime: <?= $recipe->prep_time ?> min</p>
+<p>CookTime: <?= $recipe->cook_time ?> min</p>
+<p>Servings: <?= $recipe->servings ?> </p>
+<p>Created: <?= date_format($recipe->created, "m/d/Y") ?></p>
+<p>Created By: <?= $recipe->user_id ?></p>
+<p><b>Tags:</b> <?= h($recipe->tag_string) ?></p>
+<hr>
+<div class="row" id="recipeBody">
+    <h1>Description</h1>
+    <?php foreach ($recipeBody as $bodyPart) {
+        $uppercaseFirst = ucfirst($bodyPart);
+        echo "<p>$uppercaseFirst</p>";
+    } ?>
+</div>
+
+</div>
+
+2nd column - Ingredients & Directions -->
+<!-- <div class="ui column container" id="recipeView2ndColumn">
+    <div class="row">
+        <h1>Ingredients</h1>
+        <?php foreach ($ingredients as $ingredient) {
+            $uppercaseFirst = ucfirst($ingredient);
+            echo "<p>$uppercaseFirst</p>";
+        } ?>
+    </div>
+    <br>
+    <hr>
+    <div class="row">
+        <h1>Directions</h1>
+        <?php $counter = 1; ?>
+        <?php foreach ($directions as $direction) {
+            $uppercaseFirst = ucfirst($direction);
+            echo "<b>Step $counter</b>";
+            echo "<p>$direction</p>";
+            $counter++;
+        } ?>
+    </div>
+</div>
+</div>  -->
 <br>
 
 <!-- <?php if ($creator === $recipe->user_id) : ?>
