50ad268acc81849aaaf18107ac879af4341c3ecd
Michael Wilson <mike@jumperstudios.com>
2022-08-17 14:36:30 -0500

    chore: change images count per row in projects page

--- portfolio/src/pages/projects.js
+++ portfolio/src/pages/projects.js
@@ -321,7 +321,7 @@ function Projects(props) {
 
       <div className="ui very padded relaxed grid" id="projects">
         <div
-          className="ui cards centered five medium horizontal"
+          className="ui cards centered six medium horizontal"
           id="list"
           style={{ marginLeft: "1rem", marginRight: "1rem" }}
         ></div>
